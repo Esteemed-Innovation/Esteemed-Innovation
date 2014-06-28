761bd97f55eefa271ec5cec5ffe23453e4e00da5
zenith <the.number.zero@gmail.com>
2014-06-28 19:58:17 -0400

    All the ores

--- src/main/java/flaxbeard/steamcraft/item/ItemSmashedOre.java
+++ src/main/java/flaxbeard/steamcraft/item/ItemSmashedOre.java
@@ -32,8 +32,19 @@ public class ItemSmashedOre extends Item {
 		
 		oreTypes.add(getPair("oreIron", 0xEEC1A4, "iron"));
 		oreTypes.add(getPair("oreGold", 0xF4CC00, "gold"));
-		oreTypes.add(getPair("oreCopper", 0xC14B36, "copper"));
-		oreTypes.add(getPair("oreZinc", 0xD0D0d2, "zinc"));
+		oreTypes.add(getPair("oreCopper", 0xEF9347, "copper"));
+		oreTypes.add(getPair("oreZinc", 0xFFFFFF, "zinc"));
+		oreTypes.add(getPair("oreTin", 0xFFFADD, "tin"));
+		oreTypes.add(getPair("oreNickel", 0xFFF8BA, "nickel"));
+		oreTypes.add(getPair("oreSilver", 0xE0FFFB, "silver"));
+		oreTypes.add(getPair("oreLead", 0x939BBC, "lead"));
+		oreTypes.add(getPair("oreAluminum", 0xF3FFF2, "aluminum"));
+		oreTypes.add(getPair("oreOsmium", 0x666C82, "osmium"));
+		oreTypes.add(getPair("oreCobalt", 0x3D78ED, "cobalt"));
+		oreTypes.add(getPair("oreArdite", 0xAA872A, "ardite"));
+		
+		//TODO: cobalt and ardite
+		//item.steamcraft:smashedOre.osmium.name
 		
 		// make sure we don't include things that don't exist
 		oreTypes.add(getPair("oreSnozberry",0xff00ff, "snozberry"));
