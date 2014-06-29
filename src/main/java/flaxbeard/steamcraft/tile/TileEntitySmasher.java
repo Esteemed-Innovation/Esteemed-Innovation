2286e0e1c1fd92ff19b704513e0497536e1d3107
zenith <the.number.zero@gmail.com>
2014-06-29 17:52:53 -0400

    Particles! (also failed attempt at spawning items)

--- src/main/java/flaxbeard/steamcraft/tile/TileEntitySmasher.java
+++ src/main/java/flaxbeard/steamcraft/tile/TileEntitySmasher.java
@@ -1,20 +1,18 @@
 package flaxbeard.steamcraft.tile;
 
 
-import flaxbeard.steamcraft.SteamcraftBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-
-import net.minecraft.block.Block;
-
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import flaxbeard.steamcraft.Steamcraft;
 import flaxbeard.steamcraft.SteamcraftBlocks;
 
 public class TileEntitySmasher extends TileEntity {
@@ -35,10 +33,12 @@ public class TileEntitySmasher extends TileEntity {
     	super.getDescriptionPacket();
         NBTTagCompound access = new NBTTagCompound();
         access.setInteger("spinup", spinup);
+        access.setBoolean("hasBlockUpdate", hasBlockUpdate);
         access.setFloat("extendedLength", extendedLength);
         access.setInteger("extendedTicks", extendedTicks);
         access.setInteger("block", Block.getIdFromBlock(smooshingBlock));
         access.setInteger("smooshingMeta", smooshingMeta);
+        
 
         return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 1, access);
 	}
@@ -51,6 +51,7 @@ public class TileEntitySmasher extends TileEntity {
     	this.extendedLength = access.getFloat("extendedLength");
     	this.extendedTicks = access.getInteger("extendedTicks");
     	this.spinup = access.getInteger("spinup");
+    	this.hasBlockUpdate = access.getBoolean("hasBlockUpdate");
     	this.smooshingBlock = Block.getBlockById(access.getInteger("block"));
     	this.smooshingMeta = access.getInteger("smooshingMeta");
 
@@ -58,9 +59,14 @@ public class TileEntitySmasher extends TileEntity {
     }
 	
 	public void updateEntity(){
+		int[] target = getTarget(1);
+		int x = target[0], y = yCoord, z = target[1];
+		if (extendedTicks == 3){
+			Steamcraft.instance.proxy.spawnBreakParticles(worldObj, x+0.5F, y + 0.7F, z + 0.5F, this.smooshingBlock,  (float)(Math.random()-0.5F)/12.0F, 0.0F, (float)(Math.random()-0.5F)/12.0F);
+		}
 		//handle state changes
 		if (this.hasBlockUpdate && this.hasPartner()){
-			System.out.println("Status: isActive: "+isActive+"; isBreaking: "+isBreaking+"; shouldStop: "+shouldStop);
+			//System.out.println("Status: isActive: "+isActive+"; isBreaking: "+isBreaking+"; shouldStop: "+shouldStop);
 			if (this.hasSomethingToSmash() && !this.isActive){
 				this.isActive = true;
 				this.isBreaking = true;
@@ -79,18 +85,12 @@ public class TileEntitySmasher extends TileEntity {
 			// if we haven't spun up yet, do it.
 			if (this.isBreaking){
 				if (this.spinup < 41){
-					System.out.println("Spinning up!" + spinup);
+					//System.out.println("Spinning up!" + spinup);
 					// spinup complete. SMAASH!
 					if (this.spinup == 40){
-						System.out.println("SMAAAAASH");
-						int[] target = getTarget(1);
-						int x = target[0], y = yCoord, z = target[1];
-//						Block block = worldObj.getBlock(x, y, z);
-//						Item item = block.getItem(worldObj, x, y, z);
-//						ItemStack stack = new ItemStack(block.getItem(worldObj, x, y, z), worldObj.getBlockMetadata(x, y, z));
-//						EntityItem eItem = new EntityItem(worldObj, (float) x, (float) y, (float) z, stack);
-//						eItem.setVelocity(0D, 0D, 0D);
-//						worldObj.spawnEntityInWorld(eItem);
+						//System.out.println("SMAAAAASH");
+						
+						
 						this.smooshingBlock = worldObj.getBlock(x, y, z);
 						this.smooshingMeta = worldObj.getBlockMetadata(x, y, z);
 
@@ -104,8 +104,13 @@ public class TileEntitySmasher extends TileEntity {
 				
 				// if we've spun up, extend
 				} else if (this.extendedLength < 0.5F){
-					System.out.println("Extending: "+this.extendedLength);
+					//System.out.println("Extending: "+this.extendedLength);
 					this.extendedLength += 0.1F;
+					if (this.extendedTicks == 3){
+						
+						spawnItems(x, y, z);
+						
+					}
 					this.extendedTicks++;
 				
 				// we're done extending. Time to go inactive and start retracting	
@@ -118,11 +123,11 @@ public class TileEntitySmasher extends TileEntity {
 				if (this.extendedLength > 0.0F){
 					this.extendedLength -= 0.025F;
 					this.extendedTicks++;
-					System.out.println("Retracting: "+this.extendedLength);
+					//System.out.println("Retracting: "+this.extendedLength);
 					if (this.extendedLength < 0F) this.extendedLength = 0F;
 				} else {
 					//TODO: destroy dummy block
-					System.out.println("Done!");
+					//System.out.println("Done!");
 					this.isActive = false;
 					this.extendedTicks = 0;
 				}
@@ -132,6 +137,11 @@ public class TileEntitySmasher extends TileEntity {
 			this.worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
 		}
 	
+	}
+	
+	@SideOnly(Side.SERVER)
+	private void spawnItems(int x, int y, int z){
+		
 	}
 	
 	private boolean hasSomethingToSmash() {
