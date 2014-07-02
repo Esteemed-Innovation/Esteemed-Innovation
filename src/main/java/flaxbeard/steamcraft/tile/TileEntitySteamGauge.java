cdbd182b33b3b10458c48b16903ec9f96135e183
zenith <the.number.zero@gmail.com>
2014-07-02 13:58:49 -0400

    Steam gauge can now be read by comparators

--- src/main/java/flaxbeard/steamcraft/tile/TileEntitySteamGauge.java
+++ src/main/java/flaxbeard/steamcraft/tile/TileEntitySteamGauge.java
@@ -1,7 +1,57 @@
 package flaxbeard.steamcraft.tile;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityComparator;
+import net.minecraftforge.common.util.ForgeDirection;
+import flaxbeard.steamcraft.api.ISteamTransporter;
 
 public class TileEntitySteamGauge extends TileEntity {
 
+	private int lastCompOutput = 0;
+	private ForgeDirection[] cardinals = new ForgeDirection[]{
+			ForgeDirection.NORTH,
+			ForgeDirection.SOUTH,
+			ForgeDirection.EAST,
+			ForgeDirection.WEST
+	};
+	
+	public void updateEntity(){
+		if (!worldObj.isRemote){
+			int compOutput = getComparatorOutput(); 
+			if (compOutput != lastCompOutput){
+				lastCompOutput = compOutput;
+				markDirty();
+//				for (int i = 0; i <= 3; i++){
+//					if (cardinals[i] != myDir()){
+//						ForgeDirection d = cardinals[i];
+//						worldObj.getBlock(xCoord, yCoord, zCoord);
+//						worldObj.markBlockForUpdate(xCoord + d.offsetX, yCoord, zCoord + d.offsetZ);
+//					}
+//				}
+			}
+		}
+	}
+	
+	private float getPressure(){
+		ForgeDirection d = myDir().getOpposite();
+		ISteamTransporter source = null;
+		TileEntity te = worldObj.getTileEntity(xCoord + d.offsetX, yCoord, zCoord+ d.offsetZ);
+		if (te != null && te instanceof ISteamTransporter){
+			source = (ISteamTransporter) te;
+			return source.getPressure();
+		}
+		return 0.0F;
+	}
+	
+	public int getComparatorOutput(){
+		int out = (int)(15 * (100*((double)getPressure() * 0.01D) ));
+		System.out.println(out);
+		return out;
+	}
+	
+	private ForgeDirection myDir(){
+		return ForgeDirection.getOrientation(worldObj.getBlockMetadata(xCoord, yCoord, zCoord));
+	}
+	
+	
 }
