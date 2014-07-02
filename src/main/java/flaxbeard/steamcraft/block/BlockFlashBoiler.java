f0a41dc74a54b19053ed9f86483031e90a618ab9
zenith <the.number.zero@gmail.com>
2014-07-02 01:02:38 -0400

    flash boiler drops items

--- src/main/java/flaxbeard/steamcraft/block/BlockFlashBoiler.java
+++ src/main/java/flaxbeard/steamcraft/block/BlockFlashBoiler.java
@@ -9,9 +9,11 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
@@ -35,6 +37,9 @@ import flaxbeard.steamcraft.tile.TileEntityFlashBoiler;
 
 
 public class BlockFlashBoiler extends BlockContainer{
+	
+	private final Random rand = new Random();
+	
 	public IIcon otherIcon;
 	public IIcon specialIcon;
 	
@@ -52,7 +57,7 @@ public class BlockFlashBoiler extends BlockContainer{
 	public IIcon bottomLeftO;
 	public IIcon topRightO;
 	public IIcon bottomRightO;
-	
+
 	public BlockFlashBoiler(){
 		super(Material.iron);
 		
@@ -64,10 +69,10 @@ public class BlockFlashBoiler extends BlockContainer{
 	}
 	
 	public void onBlockPreDestroy(World world, int x, int y, int z, int meta){
-		if (meta > 0){
-			TileEntityFlashBoiler te = (TileEntityFlashBoiler) world.getTileEntity(x, y, z);
-			te.destroyMultiblock();
-		}
+		//if (meta > 0){
+		//	TileEntityFlashBoiler te = (TileEntityFlashBoiler) world.getTileEntity(x, y, z);
+		//	te.destroyMultiblock();
+		//}
 		
 	}
 	
@@ -363,5 +368,61 @@ public class BlockFlashBoiler extends BlockContainer{
 	private float getRandomFlameOffset(Random random){
 		return random.nextFloat() * 0.8F - 0.4F;
 	}
+	
+	 public void breakBlock(World world, int x, int y, int z, Block block, int meta)
+	    {
+	        
+        	TileEntityFlashBoiler boiler = (TileEntityFlashBoiler)world.getTileEntity(x, y, z);
+
+            if (boiler != null)
+            {
+                for (int i1 = 0; i1 < boiler.getSizeInventory(); ++i1)
+                {
+                    ItemStack itemstack = boiler.getStackInSlot(i1);
+
+                    if (itemstack != null)
+                    {
+                        float f = this.rand.nextFloat() * 0.8F + 0.1F;
+                        float f1 = this.rand.nextFloat() * 0.8F + 0.1F;
+                        float f2 = this.rand.nextFloat() * 0.8F + 0.1F;
+
+                        while (itemstack.stackSize > 0)
+                        {
+                            int j1 = this.rand.nextInt(21) + 10;
+
+                            if (j1 > itemstack.stackSize)
+                            {
+                                j1 = itemstack.stackSize;
+                            }
+
+                            itemstack.stackSize -= j1;
+                            EntityItem entityitem = new EntityItem(world, (double)((float)x + f), (double)((float)y + f1), (double)((float)z + f2), new ItemStack(itemstack.getItem(), j1, itemstack.getItemDamage()));
+
+                            if (itemstack.hasTagCompound())
+                            {
+                                entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
+                            }
+
+                            float f3 = 0.05F;
+                            entityitem.motionX = (double)((float)this.rand.nextGaussian() * f3);
+                            entityitem.motionY = (double)((float)this.rand.nextGaussian() * f3 + 0.2F);
+                            entityitem.motionZ = (double)((float)this.rand.nextGaussian() * f3);
+                            world.spawnEntityInWorld(entityitem);
+                        }
+                    }
+                    boiler.setInventorySlotContents(i1, null);
+                    
+                }
+
+                world.func_147453_f(x, y, z, block);
+            }
+	        
+            if (meta > 0){
+				TileEntityFlashBoiler te = (TileEntityFlashBoiler) world.getTileEntity(x, y, z);
+				te.destroyMultiblock();
+			}
+	        super.breakBlock(world, x, y, z, block, meta);
+	        
+	    }
 	
 }
