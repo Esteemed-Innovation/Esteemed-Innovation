8a061153890c817170033b5bcb4a665ca8c3de5c
zenith <the.number.zero@gmail.com>
2014-07-02 23:29:31 -0400

    added try/catch just in case

--- src/main/java/flaxbeard/steamcraft/tile/TileEntitySmasher.java
+++ src/main/java/flaxbeard/steamcraft/tile/TileEntitySmasher.java
@@ -214,9 +214,15 @@ public class TileEntitySmasher extends TileEntity implements ISteamTransporter {
 						if (!worldObj.isAirBlock(x, y, z) && worldObj.getTileEntity(x, y, z) == null && worldObj.getBlock(x, y, z).getBlockHardness(worldObj, x, y, z) < 50F){
 							this.spinup++;
 							if (this.getBlockMetadata() % 2 == 0)
-								this.smooshingBlock = worldObj.getBlock(x, y, z);
-							this.smooshingMeta = worldObj.getBlockMetadata(x, y, z);
-							this.smooshedStack = new ItemStack(smooshingBlock.getItem(worldObj, x, y, z),1, smooshingMeta);
+								try{
+									this.smooshingBlock = worldObj.getBlock(x, y, z);
+									this.smooshingMeta = worldObj.getBlockMetadata(x, y, z);
+									this.smooshedStack = new ItemStack(smooshingBlock.getItem(worldObj, x, y, z),1, smooshingMeta);
+								} catch (Exception e){
+									System.out.println("================== WOULD HAVE CRASHED ==================");
+									System.out.println("This smasher's meta: "+this.getBlockMetadata());
+									e.printStackTrace();
+								}
 								worldObj.setBlock(x, y, z, SteamcraftBlocks.dummy);
 						} else {
 							//System.out.println("No block.");
