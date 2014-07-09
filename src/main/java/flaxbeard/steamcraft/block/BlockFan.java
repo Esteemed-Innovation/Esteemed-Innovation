5e0766992cd05e418f98c76b23042dc1838d2c7a
Flaxbeard <vitanovafc@gmail.com>
2014-07-08 20:11:48 -0500

    Fan

---
+++ src/main/java/flaxbeard/steamcraft/block/BlockFan.java
@@ -0,0 +1,62 @@
+package flaxbeard.steamcraft.block;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import flaxbeard.steamcraft.Steamcraft;
+import flaxbeard.steamcraft.tile.TileEntityFan;
+import flaxbeard.steamcraft.tile.TileEntitySteamHeater;
+
+public class BlockFan extends BlockContainer {
+	
+	private IIcon iconOn;
+	private IIcon iconOff;
+    public BlockFan() {
+		super(Material.iron);
+	}
+
+	public void onBlockPlacedBy(World p_149689_1_, int p_149689_2_, int p_149689_3_, int p_149689_4_, EntityLivingBase p_149689_5_, ItemStack p_149689_6_)
+    {
+        int l = determineOrientation(p_149689_1_, p_149689_2_, p_149689_3_, p_149689_4_, p_149689_5_);
+        p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, l, 2);
+    }
+    
+    public static int determineOrientation(World p_150071_0_, int p_150071_1_, int p_150071_2_, int p_150071_3_, EntityLivingBase p_150071_4_)
+    {
+        if (MathHelper.abs((float)p_150071_4_.posX - (float)p_150071_1_) < 2.0F && MathHelper.abs((float)p_150071_4_.posZ - (float)p_150071_3_) < 2.0F)
+        {
+            double d0 = p_150071_4_.posY + 1.82D - (double)p_150071_4_.yOffset;
+
+            if (d0 - (double)p_150071_2_ > 2.0D)
+            {
+                return 1;
+            }
+
+            if ((double)p_150071_2_ - d0 > 0.0D)
+            {
+                return 0;
+            }
+        }
+
+        int l = MathHelper.floor_double((double)(p_150071_4_.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        return l == 0 ? 2 : (l == 1 ? 5 : (l == 2 ? 3 : (l == 3 ? 4 : 0)));
+    }
+
+	@Override
+	public TileEntity createNewTileEntity(World var1, int var2) {
+		return new TileEntityFan();
+	}
+	
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+}
---
+++ src/main/java/flaxbeard/steamcraft/tile/TileEntityFan.java
@@ -0,0 +1,58 @@
+package flaxbeard.steamcraft.tile;
+
+import java.util.List;
+
+import flaxbeard.steamcraft.Steamcraft;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraftforge.common.util.ForgeDirection;
+
+public class TileEntityFan extends TileEntity {
+	@Override
+	public void updateEntity() {
+		int meta = this.worldObj.getBlockMetadata(xCoord, yCoord, zCoord);
+		ForgeDirection dir = ForgeDirection.getOrientation(meta);
+		int blocksInFront = 0;
+		boolean blocked = false;
+		for (int i = 1; i<6; i++) {
+			if (!this.worldObj.isRemote && this.worldObj.rand.nextInt(20) == 0 && !blocked && this.worldObj.getBlock(xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i) != Blocks.air && this.worldObj.getBlock(xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i).isReplaceable(worldObj, xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i)) {
+				int tMeta = this.worldObj.getBlockMetadata(xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i);				
+				this.worldObj.getBlock(xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i).dropBlockAsItem(worldObj, xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i, tMeta, 0);
+				for (int v = 0; v<5; v++) {
+					Steamcraft.instance.proxy.spawnBreakParticles(worldObj, xCoord+dir.offsetX*i+0.5F, yCoord+dir.offsetY*i+0.5F, zCoord+dir.offsetZ*i+0.5F, this.worldObj.getBlock(xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i), 0.0F, 0.0F, 0.0F);
+				}
+				this.worldObj.setBlockToAir(xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i);
+			}
+			if (!blocked && (this.worldObj.getBlock(xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i).isReplaceable(worldObj, xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i) || this.worldObj.isAirBlock(xCoord+dir.offsetX*i, yCoord+dir.offsetY*i, zCoord+dir.offsetZ*i))) {
+				blocksInFront = i;
+			}
+			else
+			{
+				blocked = true;
+			}
+		}
+		List entities = worldObj.getEntitiesWithinAABB(Entity.class, AxisAlignedBB.getBoundingBox(xCoord+(dir.offsetX < 0 ? dir.offsetX * blocksInFront : 0), yCoord+(dir.offsetY < 0 ? dir.offsetY * blocksInFront : 0), zCoord+(dir.offsetZ < 0 ? dir.offsetZ * blocksInFront : 0), xCoord+1+(dir.offsetX > 0 ? dir.offsetX * blocksInFront : 0), yCoord+1+(dir.offsetY > 0 ? dir.offsetY * blocksInFront : 0), zCoord+1+(dir.offsetZ > 0 ? dir.offsetZ * blocksInFront : 0)));
+		for (Object obj : entities) {
+			Entity entity = (Entity) obj;
+			if (entity.canBePushed() || entity instanceof EntityItem) {
+				if (entity instanceof EntityPlayer && ((EntityPlayer)entity).isSneaking()) {
+					entity.motionX += dir.offsetX * 0.025F;
+					entity.motionY += dir.offsetY * 0.05F;
+					entity.motionZ += dir.offsetZ * 0.025F;
+				}
+				else
+				{
+					entity.motionX += dir.offsetX * 0.075F;
+					entity.motionY += dir.offsetY * 0.1F;
+					entity.motionZ += dir.offsetZ * 0.075F;
+				}
+				entity.fallDistance = 0.0F;
+			}
+		}
+	}
+}
