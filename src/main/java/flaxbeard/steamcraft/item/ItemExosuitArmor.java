352adf1e2cbdc5b5bbc94e1c542f2dd5e7d31a06
Flaxbeard <theflaxbeard@gmail.com>
2014-07-15 19:58:02 -0500

    More exosuit fixes

--- src/main/java/flaxbeard/steamcraft/item/ItemExosuitArmor.java
+++ src/main/java/flaxbeard/steamcraft/item/ItemExosuitArmor.java
@@ -141,10 +141,10 @@ public class ItemExosuitArmor extends ItemArmor implements ISpecialArmor,IEngine
 		if (armor.hasTagCompound()) {
         	if (armor.stackTagCompound.hasKey("plate")) {
         		ExosuitPlate plate = UtilPlates.getPlate(armor.stackTagCompound.getString("plate"));
-        		return new ArmorProperties(0, plate.getDamageReductionAmount(slot,source) / 25.0D, armor.getMaxDamage() + 1 - armor.getItemDamage());
+        		return new ArmorProperties(0, plate.getDamageReductionAmount(slot,source) / 25.0D, armor.getMaxDamage());
         	}
         }
-		return new ArmorProperties(0, ItemArmor.ArmorMaterial.CLOTH.getDamageReductionAmount(3-slot) / 25.0D, armor.getMaxDamage() + 1 - armor.getItemDamage());
+		return new ArmorProperties(0, ItemArmor.ArmorMaterial.CLOTH.getDamageReductionAmount(3-slot) / 25.0D, armor.getMaxDamage());
 	}
 
 	@Override
