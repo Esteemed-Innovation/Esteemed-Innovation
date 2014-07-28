8d4b41cc6f58de58e8b532649c8bd8aef2be401a
Flaxbeard <theflaxbeard@gmail.com>
2014-07-28 13:11:38 -0500

    Removed WAILA

--- src/main/java/flaxbeard/steamcraft/integration/waila/MoldDataProvider.java
+++ src/main/java/flaxbeard/steamcraft/integration/waila/MoldDataProvider.java
@@ -1,43 +1,43 @@
-package flaxbeard.steamcraft.integration.waila;
-
-import java.util.List;
-
-import mcp.mobius.waila.api.IWailaConfigHandler;
-import mcp.mobius.waila.api.IWailaDataAccessor;
-import mcp.mobius.waila.api.IWailaDataProvider;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.item.ItemStack;
-import flaxbeard.steamcraft.tile.TileEntityCrucible;
-import flaxbeard.steamcraft.tile.TileEntityMold;
-import flaxbeard.steamcraft.tile.TileEntityValvePipe;
-
-public class MoldDataProvider implements IWailaDataProvider{
-	@Override
-	public List<String> getWailaBody(ItemStack stack, List<String> tt, IWailaDataAccessor da, IWailaConfigHandler ch) {
-		 if (da.getTileEntity() instanceof TileEntityMold){
-			 TileEntityMold  te = (TileEntityMold) da.getTileEntity();
-			 if (te.mold[0] != null) {
-				 tt.add(I18n.format("steamcraft.waila.mold") + " " + te.mold[0].getDisplayName());
-			 }
-		 }
-		 return tt;
-	}
-
-	@Override
-	public List<String> getWailaHead(ItemStack stack, List<String> tt,
-			IWailaDataAccessor da, IWailaConfigHandler cj) {
-		return tt;
-	}
-
-	@Override
-	public ItemStack getWailaStack(IWailaDataAccessor da,
-			IWailaConfigHandler ch) {
-		return null;
-	}
-
-	@Override
-	public List<String> getWailaTail(ItemStack stack, List<String> tt,
-			IWailaDataAccessor da, IWailaConfigHandler ch) {
-		return tt;
-	}
-}
+//package flaxbeard.steamcraft.integration.waila;
+//
+//import java.util.List;
+//
+//import mcp.mobius.waila.api.IWailaConfigHandler;
+//import mcp.mobius.waila.api.IWailaDataAccessor;
+//import mcp.mobius.waila.api.IWailaDataProvider;
+//import net.minecraft.client.resources.I18n;
+//import net.minecraft.item.ItemStack;
+//import flaxbeard.steamcraft.tile.TileEntityCrucible;
+//import flaxbeard.steamcraft.tile.TileEntityMold;
+//import flaxbeard.steamcraft.tile.TileEntityValvePipe;
+//
+//public class MoldDataProvider implements IWailaDataProvider{
+//	@Override
+//	public List<String> getWailaBody(ItemStack stack, List<String> tt, IWailaDataAccessor da, IWailaConfigHandler ch) {
+//		 if (da.getTileEntity() instanceof TileEntityMold){
+//			 TileEntityMold  te = (TileEntityMold) da.getTileEntity();
+//			 if (te.mold[0] != null) {
+//				 tt.add(I18n.format("steamcraft.waila.mold") + " " + te.mold[0].getDisplayName());
+//			 }
+//		 }
+//		 return tt;
+//	}
+//
+//	@Override
+//	public List<String> getWailaHead(ItemStack stack, List<String> tt,
+//			IWailaDataAccessor da, IWailaConfigHandler cj) {
+//		return tt;
+//	}
+//
+//	@Override
+//	public ItemStack getWailaStack(IWailaDataAccessor da,
+//			IWailaConfigHandler ch) {
+//		return null;
+//	}
+//
+//	@Override
+//	public List<String> getWailaTail(ItemStack stack, List<String> tt,
+//			IWailaDataAccessor da, IWailaConfigHandler ch) {
+//		return tt;
+//	}
+//}
--- src/main/java/flaxbeard/steamcraft/integration/waila/WailaIntegration.java
+++ src/main/java/flaxbeard/steamcraft/integration/waila/WailaIntegration.java
@@ -1,16 +1,16 @@
-package flaxbeard.steamcraft.integration.waila;
-
-import mcp.mobius.waila.api.IWailaRegistrar;
-import flaxbeard.steamcraft.api.ISteamTransporter;
-import flaxbeard.steamcraft.tile.TileEntityCrucible;
-import flaxbeard.steamcraft.tile.TileEntityMold;
-
-public class WailaIntegration {
-    public static void callbackRegister(IWailaRegistrar registrar)
-    {
-    	registrar.registerBodyProvider(new SteamTransporterDataProvider(), ISteamTransporter.class);
-    	registrar.registerBodyProvider(new MoldDataProvider(), TileEntityMold.class);
-    	registrar.registerBodyProvider(new CrucibleDataProvider(), TileEntityCrucible.class);
-
-    }
-}
+//package flaxbeard.steamcraft.integration.waila;
+//
+//import mcp.mobius.waila.api.IWailaRegistrar;
+//import flaxbeard.steamcraft.api.ISteamTransporter;
+//import flaxbeard.steamcraft.tile.TileEntityCrucible;
+//import flaxbeard.steamcraft.tile.TileEntityMold;
+//
+//public class WailaIntegration {
+//    public static void callbackRegister(IWailaRegistrar registrar)
+//    {
+//    	registrar.registerBodyProvider(new SteamTransporterDataProvider(), ISteamTransporter.class);
+//    	registrar.registerBodyProvider(new MoldDataProvider(), TileEntityMold.class);
+//    	registrar.registerBodyProvider(new CrucibleDataProvider(), TileEntityCrucible.class);
+//
+//    }
+//}
