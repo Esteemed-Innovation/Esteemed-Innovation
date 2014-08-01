125c60cf9bb7f44dde0119fd2d110a4b7d270b65
Zenith <the.number.zero@gmail.com>
2014-08-01 18:20:33 -0400

    Whistle sound!

--- src/main/java/flaxbeard/steamcraft/block/BlockWhistle.java
+++ src/main/java/flaxbeard/steamcraft/block/BlockWhistle.java
@@ -91,7 +91,7 @@ public class BlockWhistle extends BlockContainer {
     public int onBlockPlaced(World world, int x, int y, int z, int side, float p_149660_6_, float p_149660_7_, float p_149660_8_, int meta)
     {
     	return (meta == 1 ? side+10 : side);
-    }
+    }   
     
     @SideOnly(Side.CLIENT)
     public IIcon getIcon(int side, int meta)
