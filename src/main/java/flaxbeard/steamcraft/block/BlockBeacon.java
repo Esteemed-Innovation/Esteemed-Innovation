ab054e21ecabf0010ae49174aad118db76d15d16
Flaxbeard <theflaxbeard@gmail.com>
2014-08-01 22:04:27 -0500

    More book entries

---
+++ src/main/java/flaxbeard/steamcraft/block/BlockBeacon.java
@@ -0,0 +1,18 @@
+package flaxbeard.steamcraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.IBlockAccess;
+
+public class BlockBeacon extends Block {
+
+	public BlockBeacon(Material p_i45394_1_) {
+		super(p_i45394_1_);
+	}
+
+    public boolean isBeaconBase(IBlockAccess worldObj, int x, int y, int z, int beaconX, int beaconY, int beaconZ)
+    {
+        return true;
+    }
+}
