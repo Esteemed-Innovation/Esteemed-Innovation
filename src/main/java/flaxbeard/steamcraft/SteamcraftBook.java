b85bbc9212acab6de179585fe3f789511b104591
Zenith <the.number.zero@gmail.com>
2014-08-06 18:10:42 -0400

    Updated Steam Filler book entry

--- src/main/java/flaxbeard/steamcraft/SteamcraftBook.java
+++ src/main/java/flaxbeard/steamcraft/SteamcraftBook.java
@@ -176,7 +176,7 @@ public class SteamcraftBook {
 		SteamcraftRegistry.addResearch("research.Tank.name","category.SteamPower.name",new BookPageItem("research.Tank.name","research.Tank.0", new ItemStack(SteamcraftBlocks.tank)),new BookPageCrafting("","tank1","tank2"));
 		
 		if (Config.enableCharger){
-			SteamcraftRegistry.addResearch("research.Filler.name","category.SteamPower.name",new BookPageItem("research.Filler.name","research.Filler.0", new ItemStack(SteamcraftBlocks.charger)),new BookPageCrafting("","filler1","filler2"));
+			SteamcraftRegistry.addResearch("research.Filler.name","category.SteamPower.name",new BookPageItem("research.Filler.name","research.Filler.0", new ItemStack(SteamcraftBlocks.charger)), new BookPageText("research.Filler.name","research.Filler.1"), new BookPageCrafting("","filler1","filler2"));
 		}
 		
 		if (Config.enableHeater){
