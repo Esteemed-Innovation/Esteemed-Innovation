7085bd68cad9f3b2e38c3150d5ce7b86d88c6625
Flaxbeard <theflaxbeard@gmail.com>
2014-08-07 16:03:09 -0500

    Config.VERSION

--- src/main/java/flaxbeard/steamcraft/Config.java
+++ src/main/java/flaxbeard/steamcraft/Config.java
@@ -4,7 +4,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.config.Configuration;
 
 public class Config {
-	public static final String VERSION = "0.25.0";
+	public static final String VERSION = "0.25.1";
 	public static boolean genCopper;
 	public static boolean genZinc;
 	public static boolean passiveDrain;
