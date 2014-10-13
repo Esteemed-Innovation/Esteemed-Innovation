46d23cda8af38890c3fed473ed54d1dcf97ebf81
SatanicSanta <satanicsantagaming@gmail.com>
2014-10-12 18:53:11 -0700

    Updating version numbers

--- src/main/java/flaxbeard/steamcraft/Config.java
+++ src/main/java/flaxbeard/steamcraft/Config.java
@@ -22,7 +22,7 @@ public class Config {
 	public static final int reinforcedTankCap = 72000;
 	public static final int uberReinforcedTankCap = 144000;
 
-	public static final String VERSION = "0.26.3";
+	public static final String VERSION = "0.26.3.1";
 	public static boolean genCopper;
 	public static boolean genZinc;
 	public static boolean passiveDrain;
