647d813cbd2c7146b0692dc36292c9ec8ad63eb5
xbony2 <xbony22@gmail.com>
2014-10-21 18:56:40 -0400

    Yes, this is neccessary. Yes, this in all my other projects. Don't ask about it xD

---
+++ src/main/java/flaxbeard/steamcraft/api/util/BonyDebugger.java
@@ -0,0 +1,25 @@
+package flaxbeard.steamcraft.api.util;
+
+/**
+ * Used for xbony2's debugging purposes. This is a very serious class, do not
+ * make jokes about it.
+ * 
+ * @author xbony2
+ */
+public class BonyDebugger {
+	/**
+	 * Debugs stuff.
+	 * 
+	 * @author xbony2
+	 */
+	public static void debug() {
+		System.out.println("YOLOYOLYOYOLOYOLYOYLYOYL");
+		System.out.println("YOLOYOLYOYOLOYOLYOYLYOYL");
+		System.out.println("YOLOYOLYOYOLOYOLYOYLYOYL");
+		System.out.println("YOLOYOLYOYOLOYOLYOYLYOYL");
+		System.out.println("YOLOYOLYOYOLOYOLYOYLYOYL");
+		System.out.println("YOLOYOLYOYOLOYOLYOYLYOYL");
+		System.out.println("YOLOYOLYOYOLOYOLYOYLYOYL");
+		System.out.println("End of yolo spam.");
+	}
+}
