d063f942f9ddaaf174ef6f4b05f826f5b26774cf
xbony2 <xbony22@gmail.com>
2014-10-31 19:47:37 -0400

    Fixed Santa's shit :P

--- src/main/java/flaxbeard/steamcraft/item/ItemModularToolUpgrade.java
+++ src/main/java/flaxbeard/steamcraft/item/ItemModularToolUpgrade.java
@@ -1,7 +1,10 @@
 package flaxbeard.steamcraft.item;
 
+import net.minecraft.item.Item;
+
 /**
  * @author SatanicSanta
  */
 public class ItemModularToolUpgrade extends Item implements IModularToolUpgrade {
+	//TODO
 }
