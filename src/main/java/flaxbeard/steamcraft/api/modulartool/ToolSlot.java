5d8a7b0b52e257ef393e9ed86484b7f4245b9fe0
satanicsanta <elifosterwy@gmail.com>
2014-11-02 15:52:50 -0800

    got rid of stupid enum crap

--- src/main/java/flaxbeard/steamcraft/api/modulartool/ToolSlot.java
+++ src/main/java/flaxbeard/steamcraft/api/modulartool/ToolSlot.java
@@ -4,15 +4,12 @@ package flaxbeard.steamcraft.api.modulartool;
  * Created by elijahfoster-wysocki on 10/25/14.
  */
 public enum ToolSlot {
-    tank(5),
-    slot1(4),
-    slot2(3),
-    slot3(2),
-    slot4(1);
+    tank(0),
+    slot(1);
 
-    public int slot;
+    public int slotInt;
 
     private ToolSlot(int s){
-        slot = s;
+        slotInt = s;
     }
 }
