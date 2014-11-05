1ddc32cd5a24ba228c19520783822665c829f839
xbony2 <xbony22@gmail.com>
2014-11-05 07:43:30 -0500

    IC2 integration *should* be working. Someone test please? My workspace is sort of unstable.

---
+++ src/main/java/flaxbeard/steamcraft/integration/IC2RecipeInput.java
@@ -0,0 +1,40 @@
+package flaxbeard.steamcraft.integration;
+
+import java.util.Arrays;
+import java.util.List;
+
+import ic2.api.recipe.IRecipeInput;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Part/parts of this code was taken from IndustrialEx, which was taken from Bluepower, I 
+ * love the compact way they handle ic2's recipe input stuff. Original class comes from 
+ * <a href="https://github.com/Qmunity/BluePower/blob/master/src/main/java/com/bluepowermod/compat/ic2/IC2RecipeInput.java">here</a>.
+ * IndustrialEx's class <a href="https://github.com/xbony2/IndustrialEx/blob/master/src/main/java/xbony2/IndustrialEx/api/recipe/IC2RecipeInput.java">here</a>.
+ * 
+ * @author xbony2, MineMaarten
+ *
+ */
+public class IC2RecipeInput implements IRecipeInput{
+	private final ItemStack input;
+
+	public IC2RecipeInput(ItemStack input){
+        this.input = input;
+    }
+
+	@Override
+    public boolean matches(ItemStack subject){
+        return subject != null && input.isItemEqual(subject) && input.getItemDamage() == subject.getItemDamage();
+    }
+
+    @Override
+    public int getAmount(){
+        return input.stackSize;
+    }
+
+    @Override
+    public List<ItemStack> getInputs(){
+    	List<ItemStack> list = Arrays.asList(input);
+        return list;
+    }
+}
--- src/main/java/flaxbeard/steamcraft/integration/IndustrialCraftIntegration.java
+++ src/main/java/flaxbeard/steamcraft/integration/IndustrialCraftIntegration.java
@@ -1,5 +1,25 @@
 package flaxbeard.steamcraft.integration;
 
-public class IndustrialCraftIntegration {
+import ic2.api.item.IC2Items;
+import ic2.api.recipe.Recipes;
+import net.minecraft.item.ItemStack;
+import flaxbeard.steamcraft.SteamcraftItems;
 
+public class IndustrialCraftIntegration {
+	public static void addIC2Recipes(){
+		IC2RecipeInput tincan = new IC2RecipeInput(IC2Items.getItem("tinCan"));
+		ItemStack filled = IC2Items.getItem("filledTinCan");
+		
+		IC2RecipeInput input1 = new IC2RecipeInput(new ItemStack(SteamcraftItems.steamedBeef));
+		Recipes.cannerBottle.addRecipe(tincan, input1, filled);
+		
+		IC2RecipeInput input2 = new IC2RecipeInput(new ItemStack(SteamcraftItems.steamedChicken));
+		Recipes.cannerBottle.addRecipe(tincan, input2, filled);
+		
+		IC2RecipeInput input3 = new IC2RecipeInput(new ItemStack(SteamcraftItems.steamedFish));
+		Recipes.cannerBottle.addRecipe(tincan, input3, filled);
+		
+		IC2RecipeInput input4 = new IC2RecipeInput(new ItemStack(SteamcraftItems.steamedPorkchop));
+		Recipes.cannerBottle.addRecipe(tincan, input4, filled);
+	}
 }
