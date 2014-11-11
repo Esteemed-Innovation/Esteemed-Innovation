782d1e939db7f8eed7b6520bb3a47aa572fb47e2
xbony2 <xbony22@gmail.com>
2014-11-11 09:42:58 -0500

    Fixes #152 - IC2 foods are now proper

--- src/main/java/flaxbeard/steamcraft/integration/IndustrialCraftIntegration.java
+++ src/main/java/flaxbeard/steamcraft/integration/IndustrialCraftIntegration.java
@@ -7,19 +7,20 @@ import flaxbeard.steamcraft.SteamcraftItems;
 
 public class IndustrialCraftIntegration {
 	public static void addIC2Recipes(){
-		IC2RecipeInput tincan = new IC2RecipeInput(IC2Items.getItem("tinCan"));
-		ItemStack filled = IC2Items.getItem("filledTinCan");
+		IC2RecipeInput tincanx10 = new IC2RecipeInput(new ItemStack(IC2Items.getItem("tinCan").getItem(), 10));
+		IC2RecipeInput tincanx8 = new IC2RecipeInput(new ItemStack(IC2Items.getItem("tinCan").getItem(), 8));
+		IC2RecipeInput tincanx7 = new IC2RecipeInput(new ItemStack(IC2Items.getItem("tinCan").getItem(), 7));
 		
 		IC2RecipeInput input1 = new IC2RecipeInput(new ItemStack(SteamcraftItems.steamedBeef));
-		Recipes.cannerBottle.addRecipe(tincan, input1, filled);
+		Recipes.cannerBottle.addRecipe(tincanx10, input1, new ItemStack(IC2Items.getItem("filledTinCan").getItem(), 10));
 		
 		IC2RecipeInput input2 = new IC2RecipeInput(new ItemStack(SteamcraftItems.steamedChicken));
-		Recipes.cannerBottle.addRecipe(tincan, input2, filled);
+		Recipes.cannerBottle.addRecipe(tincanx8, input2, new ItemStack(IC2Items.getItem("filledTinCan").getItem(), 8));
 		
 		IC2RecipeInput input3 = new IC2RecipeInput(new ItemStack(SteamcraftItems.steamedFish));
-		Recipes.cannerBottle.addRecipe(tincan, input3, filled);
+		Recipes.cannerBottle.addRecipe(tincanx7, input3, new ItemStack(IC2Items.getItem("filledTinCan").getItem(), 7));
 		
 		IC2RecipeInput input4 = new IC2RecipeInput(new ItemStack(SteamcraftItems.steamedPorkchop));
-		Recipes.cannerBottle.addRecipe(tincan, input4, filled);
+		Recipes.cannerBottle.addRecipe(tincanx10, input4, new ItemStack(IC2Items.getItem("filledTinCan").getItem(), 10));
 	}
 }
