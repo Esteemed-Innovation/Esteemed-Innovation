b8cf53c8250f2a43d900cba4566bb5b2c6fdf88c
Eli Foster <elifosterwy@gmail.com>
2015-03-09 22:30:27 -0700

    Should fix #224

--- src/main/java/flaxbeard/steamcraft/world/SteamcraftOreGen.java
+++ src/main/java/flaxbeard/steamcraft/world/SteamcraftOreGen.java
@@ -1,9 +1,13 @@
 package flaxbeard.steamcraft.world;
 
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.IWorldGenerator;
 import flaxbeard.steamcraft.Config;
 import flaxbeard.steamcraft.SteamcraftBlocks;
+import flaxbeard.steamcraft.api.util.SPLog;
+
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -18,28 +22,41 @@ public class SteamcraftOreGen implements IWorldGenerator {
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
         String[] zincArray = Config.zincDims.split(";");
         final int[] zincIDs = new int[zincArray.length];
+        for (int i = 0; i < zincArray.length; i++) {
+            zincIDs[i] = Integer.parseInt(zincArray[i]);
+        }
 
         String[] copperArray = Config.copperDims.split(";");
         final int[] copperIDs = new int[copperArray.length];
+        for (int i = 0; i < copperArray.length; i++) {
+            copperIDs[i] = Integer.parseInt(copperArray[i]);
+        }
 
         switch (world.provider.dimensionId) {
-            case -1:
+            case -1: {
                 generateNether(world, random, chunkX * 16, chunkZ * 16);
                 break;
-            case 0:
+            }
+            case 0: {
                 generateSurface(world, random, chunkX * 16, chunkZ * 16);
                 break;
-            case 1:
+            }
+            case 1: {
                 generateEnd(world, random, chunkX * 16, chunkZ * 16);
                 break;
+            }
         }
 
         for (int id : copperIDs) {
-            generateExtra(world, random, chunkX * 16, chunkZ * 16, id);
+            if (id == world.provider.dimensionId) {
+                generateExtra(world, random, chunkX * 16, chunkZ * 16, id);
+            }
         }
 
         for (int id : zincIDs) {
-            generateExtra(world, random, chunkX * 16, chunkZ * 16, id);
+            if (id == world.provider.dimensionId) {
+                generateExtra(world, random, chunkX * 16, chunkZ * 16, id);
+            }
         }
     }
 
@@ -104,24 +121,35 @@ public class SteamcraftOreGen implements IWorldGenerator {
     }
 
     private void generateExtra(World world, Random random, int i, int j, int id) {
-        world.provider.dimensionId = id;
-
-        if (Config.genZincExtras) {
-            for (int k = 0; k < 10; k++) {
-                int x = i + random.nextInt(16);
-                int y = random.nextInt(128);
-                int z = j + random.nextInt(16);
-                (new WorldGenMinable(SteamcraftBlocks.steamcraftOre, 1, 7, Blocks.stone)).generate(world, random, x, y, z);
+        if (checkConfigForInvalidIntegers(id)) {
+            if (Config.genZincExtras) {
+                for (int k = 0; k < 10; k++) {
+                    int x = i + random.nextInt(16);
+                    int y = random.nextInt(128);
+                    int z = j + random.nextInt(16);
+                    (new WorldGenMinable(SteamcraftBlocks.steamcraftOre, 1, 7, Blocks.stone)).generate(world, random, x, y, z);
+                }
             }
+
+            if (Config.genCopperExtras) {
+                for (int k = 0; k < 10; k++) {
+                    int x = i + random.nextInt(16);
+                    int y = random.nextInt(128);
+                    int z = j + random.nextInt(16);
+                    (new WorldGenMinable(SteamcraftBlocks.steamcraftOre, 0, 7, Blocks.stone)).generate(world, random, x, y, z);
+                }
+            }
+        //} else {
+        //    String error = "THE FLAXBEARD'S STEAM POWER CONFIG OPTION FOR ADDITIONAL DIMENSIONS TO GENERATE ZINC AND COPPER CANNOT HAVE 0, -1, OR 1. PLEASE FIX THIS, THEN CONTINUE. THANK YOU.";
+        //    FMLLog.bigWarning(error);
         }
+    }
 
-        if (Config.genCopperExtras) {
-            for (int k = 0; k < 10; k++) {
-                int x = i + random.nextInt(16);
-                int y = random.nextInt(128);
-                int z = j + random.nextInt(16);
-                (new WorldGenMinable(SteamcraftBlocks.steamcraftOre, 0, 7, Blocks.stone)).generate(world, random, x, y, z);
-            }
+    public boolean checkConfigForInvalidIntegers(int id) {
+        if (id != 0 && id != -1 && id != 1) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
