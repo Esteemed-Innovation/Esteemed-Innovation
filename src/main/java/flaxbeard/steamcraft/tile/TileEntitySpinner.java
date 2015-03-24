867199a956f9db2071891d79701ab5ca8b9dd1c7
Eli Foster <elifosterwy@gmail.com>
2015-03-23 21:57:59 -0700

    Still cant get this to work

--- src/main/java/flaxbeard/steamcraft/tile/TileEntitySpinner.java
+++ src/main/java/flaxbeard/steamcraft/tile/TileEntitySpinner.java
@@ -4,13 +4,13 @@ import flaxbeard.steamcraft.Config;
 import flaxbeard.steamcraft.api.tile.SteamTransporterTileEntity;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockPistonBase;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntitySpinner extends SteamTransporterTileEntity {
 
 	private boolean isPowered = false;
-	private int spinTicks = 0;
-	private int cost      = Config.spinnerConsumption;
+	private int cost = Config.spinnerConsumption;
 
 	@Override
 	public boolean canInsert(ForgeDirection dir) {
@@ -28,39 +28,38 @@ public class TileEntitySpinner extends SteamTransporterTileEntity {
 		int x = xCoord;
 		int y = yCoord;
 		int z = zCoord;
-        ForgeDirection axis = ForgeDirection.WEST;
+
+		ForgeDirection never = ForgeDirection.NORTH;
+		ForgeDirection eat = ForgeDirection.EAST;
+		ForgeDirection soggy = ForgeDirection.SOUTH;
+		ForgeDirection waffles = ForgeDirection.WEST;
+		ForgeDirection[] nesw = {never, eat, soggy, waffles};
+
+		Block blockAbove = worldObj.getBlock(x, y + 1, z);
 
 		if (worldObj.isBlockIndirectlyGettingPowered(x, y, z)) {
 			isPowered = true;
 		}
 
-		if (isPowered && !worldObj.isRemote && getSteamShare() > cost) {
-			Block blockAboveSpinner = worldObj.getBlock(x, y + 1, z);
-
-			if (blockAboveSpinner.getValidRotations(worldObj, x, y, z) != null) {
+		if (isPowered && !worldObj.isRemote && getSteamShare() > cost && blockAbove != null &&
+		  blockAbove != null && !blockAbove.isAir(worldObj, x, y, z)) {
+			if (blockAbove.getValidRotations(worldObj, x, y, z) != null) {
 				System.out.println(
-				  String.format("%1$s can rotate.", blockAboveSpinner.getLocalizedName()));
+				  String.format("%1$s can rotate.", blockAbove.getLocalizedName()));
 
-				spinTicks++;
-				if (blockAboveSpinner.rotateBlock(worldObj, x, y, z, axis)) {
-					System.out.println("Block was rotated.");
+				if (blockAbove.rotateBlock(worldObj, x, y, z, never)) {
+					worldObj.markBlockForUpdate(x, y + 1, z);
+					System.out.println(String.format("Block was rotated to %1$s.", never));
 					this.decrSteam(cost);
 					isPowered = false;
 				} else {
+					isPowered = false;
 					System.out.println("Block was not rotated.");
 				}
 			} else {
 				System.out.println(
-				  String.format("%1$s cannot rotate.", blockAboveSpinner.getLocalizedName()));
+				  String.format("%1$s cannot rotate.", blockAbove.getLocalizedName()));
 			}
-		}
-
-		if (isPowered) {
-			spinTicks = 0;
-		}
-
-		if (spinTicks > 15) {
-			isPowered = false;
 		}
 	}
 }
