761a5797eeb6dfb0d55b6103395c76fdb7d55294
xbony2 <xbony22@gmail.com>
2015-06-20 09:26:55 -0400

    that was easy. Now I must add ccc/nei stuff, then I am free

--- src/main/java/flaxbeard/steamcraft/integration/TwilightForestIntegration.java
+++ src/main/java/flaxbeard/steamcraft/integration/TwilightForestIntegration.java
@@ -6,26 +6,16 @@ import flaxbeard.steamcraft.SteamcraftRecipes;
 import flaxbeard.steamcraft.api.CrucibleLiquid;
 import flaxbeard.steamcraft.api.SteamcraftRegistry;
 import flaxbeard.steamcraft.api.exosuit.ExosuitPlate;
+
+import twilightforest.item.TFItems;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class TwilightForestIntegration {
 
-    public static Item fieryIngot;
-    public static Item alphaFur;
-
-    public static void grabItems() {
-        try {
-            Class clazz = Class.forName("twilightforest.item.TFItems");
-            fieryIngot = (Item) clazz.getDeclaredField("fieryIngot").get(clazz);
-            alphaFur = (Item) clazz.getDeclaredField("alphaFur").get(clazz);
-        } catch (Exception ex) {
-            // Should probably log something here
-        }
-    }
-
-    public static void addTwilightForestLiquid() {
-        CrucibleLiquid liquidFiery = new CrucibleLiquid("fiery", new ItemStack(fieryIngot), new ItemStack(SteamcraftItems.steamcraftPlate, 1, 8), null, null, 91, 69, 69);
+    public static void postInit() {
+        CrucibleLiquid liquidFiery = new CrucibleLiquid("fiery", new ItemStack(TFItems.fieryIngot), new ItemStack(SteamcraftItems.steamcraftPlate, 1, 8), null, null, 91, 69, 69);
         SteamcraftRegistry.liquids.add(liquidFiery);
 
         SteamcraftRegistry.registerSmeltThingOredict("fieryIngot", liquidFiery, 9);
@@ -38,7 +28,7 @@ public class TwilightForestIntegration {
         }
         if (Config.enableYetiPlate) {
             SteamcraftRegistry.addExosuitPlate(new ExosuitPlate("Yeti", new ItemStack(SteamcraftItems.exosuitPlate, 1, 9), "Yeti", "Yeti", "steamcraft.plate.yeti"));
-            SteamcraftRecipes.addExosuitPlateRecipes("exoYeti", new ItemStack(alphaFur), new ItemStack(SteamcraftItems.exosuitPlate, 1, 9));
+            SteamcraftRecipes.addExosuitPlateRecipes("exoYeti", new ItemStack(TFItems.alphaFur), new ItemStack(SteamcraftItems.exosuitPlate, 1, 9));
         }
     }
 }
