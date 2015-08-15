49e00b91ba41be25a57dea5fbc3d397d7f66ad14
PaperTriangle <leonard.j.ellis@gmail.com>
2015-08-16 01:43:48 +1000

    Update EntityMusketBall.java

--- src/main/java/flaxbeard/steamcraft/entity/EntityMusketBall.java
+++ src/main/java/flaxbeard/steamcraft/entity/EntityMusketBall.java
@@ -151,8 +151,7 @@ public class EntityMusketBall extends Entity implements IProjectile {
     {
         super.onUpdate();
 
-        if (this.prevRotationPitch == 0.0F && this.prevRotationYaw == 0.0F)
-        {
+        if (this.prevRotationPitch == 0.0F && this.prevRotationYaw == 0.0F) {
             float f = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
             this.prevRotationYaw = this.rotationYaw = (float)(Math.atan2(this.motionX, this.motionZ) * 180.0D / Math.PI);
             this.prevRotationPitch = this.rotationPitch = (float)(Math.atan2(this.motionY, (double)f) * 180.0D / Math.PI);
@@ -160,37 +159,29 @@ public class EntityMusketBall extends Entity implements IProjectile {
 
         Block block = this.worldObj.getBlock(this.xTile, this.yTile, this.zTile);
 
-        if (block.getMaterial() != Material.air)
-        {
+        if (block.getMaterial() != Material.air) {
             block.setBlockBoundsBasedOnState(this.worldObj, this.xTile, this.yTile, this.zTile);
             AxisAlignedBB axisalignedbb = block.getCollisionBoundingBoxFromPool(this.worldObj, this.xTile, this.yTile, this.zTile);
 
-            if (axisalignedbb != null && axisalignedbb.isVecInside(Vec3.createVectorHelper(this.posX, this.posY, this.posZ)))
-            {
+            if (axisalignedbb != null && axisalignedbb.isVecInside(Vec3.createVectorHelper(this.posX, this.posY, this.posZ))) {
                 this.inGround = true;
             }
         }
 
-        if (this.arrowShake > 0)
-        {
+        if (this.arrowShake > 0) {
             --this.arrowShake;
         }
 
-        if (this.inGround)
-        {
+        if (this.inGround) {
             int j = this.worldObj.getBlockMetadata(this.xTile, this.yTile, this.zTile);
 
-            if (block == this.inTile && j == this.inData)
-            {
+            if (block == this.inTile && j == this.inData) {
                 ++this.ticksInGround;
 
-                if (this.ticksInGround == 20)
-                {
+                if (this.ticksInGround == 20) {
                     this.setDead();
                 }
-            }
-            else
-            {
+            } else {
                 this.inGround = false;
                 this.motionX *= (double)(this.rand.nextFloat() * 0.2F);
                 this.motionY *= (double)(this.rand.nextFloat() * 0.2F);
@@ -198,9 +189,7 @@ public class EntityMusketBall extends Entity implements IProjectile {
                 this.ticksInGround = 0;
                 this.ticksInAir = 0;
             }
-        }
-        else
-        {
+        } else {
             ++this.ticksInAir;
             Vec3 vec31 = Vec3.createVectorHelper(this.posX, this.posY, this.posZ);
             Vec3 vec3 = Vec3.createVectorHelper(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
@@ -208,8 +197,7 @@ public class EntityMusketBall extends Entity implements IProjectile {
             vec31 = Vec3.createVectorHelper(this.posX, this.posY, this.posZ);
             vec3 = Vec3.createVectorHelper(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
 
-            if (movingobjectposition != null)
-            {
+            if (movingobjectposition != null) {
                 vec3 = Vec3.createVectorHelper(movingobjectposition.hitVec.xCoord, movingobjectposition.hitVec.yCoord, movingobjectposition.hitVec.zCoord);
             }
 
@@ -219,22 +207,18 @@ public class EntityMusketBall extends Entity implements IProjectile {
             int i;
             float f1;
 
-            for (i = 0; i < list.size(); ++i)
-            {
+            for (i = 0; i < list.size(); ++i) {
                 Entity entity1 = (Entity)list.get(i);
 
-                if (entity1.canBeCollidedWith() && (entity1 != this.shootingEntity || this.ticksInAir >= 5))
-                {
+                if (entity1.canBeCollidedWith() && (entity1 != this.shootingEntity || this.ticksInAir >= 5)) {
                     f1 = 0.3F;
                     AxisAlignedBB axisalignedbb1 = entity1.boundingBox.expand((double)f1, (double)f1, (double)f1);
                     MovingObjectPosition movingobjectposition1 = axisalignedbb1.calculateIntercept(vec31, vec3);
 
-                    if (movingobjectposition1 != null)
-                    {
+                    if (movingobjectposition1 != null) {
                         double d1 = vec31.distanceTo(movingobjectposition1.hitVec);
 
-                        if (d1 < d0 || d0 == 0.0D)
-                        {
+                        if (d1 < d0 || d0 == 0.0D) {
                             entity = entity1;
                             d0 = d1;
                         }
@@ -242,17 +226,14 @@ public class EntityMusketBall extends Entity implements IProjectile {
                 }
             }
 
-            if (entity != null)
-            {
+            if (entity != null) {
                 movingobjectposition = new MovingObjectPosition(entity);
             }
 
-            if (movingobjectposition != null && movingobjectposition.entityHit != null && movingobjectposition.entityHit instanceof EntityPlayer)
-            {
+            if (movingobjectposition != null && movingobjectposition.entityHit != null && movingobjectposition.entityHit instanceof EntityPlayer) {
                 EntityPlayer entityplayer = (EntityPlayer)movingobjectposition.entityHit;
 
-                if (entityplayer.capabilities.disableDamage || this.shootingEntity instanceof EntityPlayer && !((EntityPlayer)this.shootingEntity).canAttackPlayer(entityplayer))
-                {
+                if (entityplayer.capabilities.disableDamage || this.shootingEntity instanceof EntityPlayer && !((EntityPlayer)this.shootingEntity).canAttackPlayer(entityplayer)) {
                     movingobjectposition = null;
                 }
             }
@@ -260,64 +241,49 @@ public class EntityMusketBall extends Entity implements IProjectile {
             float f2;
             float f4;
 
-            if (movingobjectposition != null)
-            {
-                if (movingobjectposition.entityHit != null)
-                {
+            if (movingobjectposition != null) {
+                if (movingobjectposition.entityHit != null) {
                     f2 = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionY * this.motionY + this.motionZ * this.motionZ);
                     int k = (int)this.damage;
 
                     DamageSource damagesource = null;
 
-                    if (this.shootingEntity == null || this.silenced)
-                    {
+                    if (this.shootingEntity == null || this.silenced) {
                         damagesource = DamageSource.causeThrownDamage(this, this);
-                    }
-                    else
-                    {
+                    } else {
                         damagesource = DamageSource.causeThrownDamage(this, this.shootingEntity);
                     }
 
-                    if (this.isBurning() && !(movingobjectposition.entityHit instanceof EntityEnderman))
-                    {
+                    if (this.isBurning() && !(movingobjectposition.entityHit instanceof EntityEnderman)) {
                         movingobjectposition.entityHit.setFire(5);
                     }
 
-                    if (movingobjectposition.entityHit.attackEntityFrom(damagesource, (float)k))
-                    {
-                        if (movingobjectposition.entityHit instanceof EntityLivingBase)
-                        {
+                    if (movingobjectposition.entityHit.attackEntityFrom(damagesource, (float)k)) {
+                        if (movingobjectposition.entityHit instanceof EntityLivingBase) {
                             EntityLivingBase entitylivingbase = (EntityLivingBase)movingobjectposition.entityHit;
 
-                            if (this.knockbackStrength > 0)
-                            {
+                            if (this.knockbackStrength > 0) {
                                 f4 = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
 
-                                if (f4 > 0.0F)
-                                {
+                                if (f4 > 0.0F) {
                                     movingobjectposition.entityHit.addVelocity(this.motionX * (double)this.knockbackStrength * 0.2000000238418579D / (double)f4, 0.1D, this.motionZ * (double)this.knockbackStrength * 0.2000000238418579D / (double)f4);
                                 }
                             }
 
-                            if (this.shootingEntity != null && this.shootingEntity instanceof EntityLivingBase)
-                            {
+                            if (this.shootingEntity != null && this.shootingEntity instanceof EntityLivingBase) {
                                 EnchantmentHelper.func_151384_a(entitylivingbase, this.shootingEntity);
                                 EnchantmentHelper.func_151385_b((EntityLivingBase)this.shootingEntity, entitylivingbase);
                             }
 
-                            if (this.shootingEntity != null && movingobjectposition.entityHit != this.shootingEntity && movingobjectposition.entityHit instanceof EntityPlayer && this.shootingEntity instanceof EntityPlayerMP)
-                            {
+                            if (this.shootingEntity != null && movingobjectposition.entityHit != this.shootingEntity && movingobjectposition.entityHit instanceof EntityPlayer && this.shootingEntity instanceof EntityPlayerMP) {
                                 ((EntityPlayerMP)this.shootingEntity).playerNetServerHandler.sendPacket(new S2BPacketChangeGameState(6, 0.0F));
                             }
                         }
 
-                        if (!(movingobjectposition.entityHit instanceof EntityEnderman))
-                        {
+                        if (!(movingobjectposition.entityHit instanceof EntityEnderman)) {
                             this.setDead();
                         }
-                    }
-                    else
-                    {
+                    } else {
                         this.motionX *= -0.10000000149011612D;
                         this.motionY *= -0.10000000149011612D;
                         this.motionZ *= -0.10000000149011612D;
@@ -325,9 +291,7 @@ public class EntityMusketBall extends Entity implements IProjectile {
                         this.prevRotationYaw += 180.0F;
                         this.ticksInAir = 0;
                     }
-                }
-                else
-                {
+                } else {
                     this.xTile = movingobjectposition.blockX;
                     this.yTile = movingobjectposition.blockY;
                     this.zTile = movingobjectposition.blockZ;
@@ -340,14 +304,13 @@ public class EntityMusketBall extends Entity implements IProjectile {
                     this.posX -= this.motionX / (double)f2 * 0.05000000074505806D;
                     this.posY -= this.motionY / (double)f2 * 0.05000000074505806D;
                     this.posZ -= this.motionZ / (double)f2 * 0.05000000074505806D;
+                    this.inGround = true;
+                    this.arrowShake = 7;
                     if ((this.inTile != null || this.inTile != Blocks.air) && !this.worldObj.isRemote) {
                     	Steamcraft.instance.proxy.blockHitEffect(this.xTile, this.yTile, this.zTile, movingobjectposition);
                     }
-                    this.inGround = true;
-                    this.arrowShake = 7;
 
-                    if (this.inTile.getMaterial() != Material.air)
-                    {
+                    if (this.inTile.getMaterial() != Material.air) {
                         this.inTile.onEntityCollidedWithBlock(this.worldObj, this.xTile, this.yTile, this.zTile, this);
                     }
                 }
@@ -360,23 +323,19 @@ public class EntityMusketBall extends Entity implements IProjectile {
             f2 = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
             this.rotationYaw = (float)(Math.atan2(this.motionX, this.motionZ) * 180.0D / Math.PI);
 
-            for (this.rotationPitch = (float)(Math.atan2(this.motionY, (double)f2) * 180.0D / Math.PI); this.rotationPitch - this.prevRotationPitch < -180.0F; this.prevRotationPitch -= 360.0F)
-            {
+            for (this.rotationPitch = (float)(Math.atan2(this.motionY, (double)f2) * 180.0D / Math.PI); this.rotationPitch - this.prevRotationPitch < -180.0F; this.prevRotationPitch -= 360.0F) {
                 ;
             }
 
-            while (this.rotationPitch - this.prevRotationPitch >= 180.0F)
-            {
+            while (this.rotationPitch - this.prevRotationPitch >= 180.0F) {
                 this.prevRotationPitch += 360.0F;
             }
 
-            while (this.rotationYaw - this.prevRotationYaw < -180.0F)
-            {
+            while (this.rotationYaw - this.prevRotationYaw < -180.0F) {
                 this.prevRotationYaw -= 360.0F;
             }
 
-            while (this.rotationYaw - this.prevRotationYaw >= 180.0F)
-            {
+            while (this.rotationYaw - this.prevRotationYaw >= 180.0F) {
                 this.prevRotationYaw += 360.0F;
             }
 
@@ -385,10 +344,8 @@ public class EntityMusketBall extends Entity implements IProjectile {
             float f3 = 0.99F;
             f1 = 0.05F;
 
-            if (this.isInWater())
-            {
-                for (int l = 0; l < 4; ++l)
-                {
+            if (this.isInWater()) {
+                for (int l = 0; l < 4; ++l) {
                     f4 = 0.25F;
                     this.worldObj.spawnParticle("bubble", this.posX - this.motionX * (double)f4, this.posY - this.motionY * (double)f4, this.posZ - this.motionZ * (double)f4, this.motionX, this.motionY, this.motionZ);
                 }
@@ -396,8 +353,7 @@ public class EntityMusketBall extends Entity implements IProjectile {
                 f3 = 0.8F;
             }
 
-            if (this.isWet())
-            {
+            if (this.isWet()) {
                 this.extinguish();
             }
 
