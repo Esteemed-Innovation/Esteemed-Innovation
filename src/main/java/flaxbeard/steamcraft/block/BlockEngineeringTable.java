91bcbc3b595a5e45f3ab351757ebc244586eda91
elifoster <elifosterwy@gmail.com>
2016-06-21 12:38:52 -0700

    :bug: :fu: Engineering Table stuff compiles

--- src/main/java/flaxbeard/steamcraft/block/BlockEngineeringTable.java
+++ src/main/java/flaxbeard/steamcraft/block/BlockEngineeringTable.java
@@ -4,46 +4,30 @@ import flaxbeard.steamcraft.Steamcraft;
 import flaxbeard.steamcraft.tile.TileEntityEngineeringTable;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockEngineeringTable extends BlockContainer {
-
-    @SideOnly(Side.CLIENT)
-    private IIcon top;
-    private IIcon bottom;
-
     public BlockEngineeringTable() {
-        super(Material.rock);
+        super(Material.ROCK);
         setHardness(3.5F);
     }
 
     @Override
-    @SideOnly(Side.CLIENT)
-    public IIcon getIcon(int side, int meta) {
-        return side == 1 ? this.top : (side == 0 ? this.bottom : this.blockIcon);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(IIconRegister p_149651_1_) {
-        this.blockIcon = p_149651_1_.registerIcon("furnace_side");
-        this.bottom = p_149651_1_.registerIcon("furnace_top");
-        this.top = p_149651_1_.registerIcon(this.getTextureName() + "_top");
-    }
-
-    @Override
-    public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9) {
-        if (par1World.isRemote) {
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack held, EnumFacing side, float hitX, float hitY, float hitZ) {
+        if (world.isRemote) {
             return true;
         } else {
-            TileEntityEngineeringTable tileentityfurnace = (TileEntityEngineeringTable) par1World.getTileEntity(par2, par3, par4);
+            TileEntity tileentity = world.getTileEntity(pos);
 
-            if (tileentityfurnace != null) {
-                par5EntityPlayer.openGui(Steamcraft.instance, 2, par1World, par2, par3, par4);
+            if (tileentity != null && tileentity instanceof TileEntityEngineeringTable) {
+                player.openGui(Steamcraft.instance, 2, world, pos.getX(), pos.getY(), pos.getZ());
             }
 
             return true;
--- src/main/java/flaxbeard/steamcraft/tile/TileEntityEngineeringTable.java
+++ src/main/java/flaxbeard/steamcraft/tile/TileEntityEngineeringTable.java
@@ -3,45 +3,45 @@ package flaxbeard.steamcraft.tile;
 import flaxbeard.steamcraft.api.IEngineerable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.text.ITextComponent;
 
 public class TileEntityEngineeringTable extends TileEntity implements IInventory {
-
-    private ItemStack[] furnaceItemStacks = new ItemStack[1];
-
-
+    private ItemStack[] contents = new ItemStack[1];
+    
     @Override
-    public void readFromNBT(NBTTagCompound par1NBTTagCompound) {
-        super.readFromNBT(par1NBTTagCompound);
-        NBTTagList nbttaglist = (NBTTagList) par1NBTTagCompound.getTag("Items");
-        this.furnaceItemStacks = new ItemStack[this.getSizeInventory()];
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+        NBTTagList nbttaglist = (NBTTagList) compound.getTag("Items");
+        contents = new ItemStack[getSizeInventory()];
 
         NBTTagCompound nbttagcompound1 = nbttaglist.getCompoundTagAt(0);
         byte b0 = nbttagcompound1.getByte("Slot");
 
-        if (b0 >= 0 && b0 < this.furnaceItemStacks.length) {
-            this.furnaceItemStacks[b0] = ItemStack.loadItemStackFromNBT(nbttagcompound1);
+        if (b0 >= 0 && b0 < contents.length) {
+            contents[b0] = ItemStack.loadItemStackFromNBT(nbttagcompound1);
         }
-
     }
 
     @Override
-    public void writeToNBT(NBTTagCompound par1NBTTagCompound) {
-        super.writeToNBT(par1NBTTagCompound);
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
         NBTTagList nbttaglist = new NBTTagList();
 
-        if (this.furnaceItemStacks[0] != null) {
+        if (contents[0] != null) {
             NBTTagCompound nbttagcompound1 = new NBTTagCompound();
             nbttagcompound1.setByte("Slot", (byte) 0);
-            this.furnaceItemStacks[0].writeToNBT(nbttagcompound1);
+            contents[0].writeToNBT(nbttagcompound1);
             nbttaglist.appendTag(nbttagcompound1);
         }
 
+        compound.setTag("Items", nbttaglist);
 
-        par1NBTTagCompound.setTag("Items", nbttaglist);
+        return compound;
     }
 
     @Override
@@ -50,14 +50,16 @@ public class TileEntityEngineeringTable extends TileEntity implements IInventory
     }
 
     @Override
-    public ItemStack getStackInSlot(int var1) {
-        if (var1 == 0) {
-            return furnaceItemStacks[var1];
+    public ItemStack getStackInSlot(int index) {
+        if (index == 0) {
+            return contents[index];
         } else {
-            if (this.getStackInSlot(0) != null) {
-                if (this.getStackInSlot(0).getItem() instanceof IEngineerable) {
-                    IEngineerable item = (IEngineerable) this.getStackInSlot(0).getItem();
-                    return item.getStackInSlot(this.getStackInSlot(0), var1);
+            ItemStack stackInSlotZero = getStackInSlot(0);
+            if (stackInSlotZero != null) {
+                Item itemInSlotZero = stackInSlotZero.getItem();
+                if (itemInSlotZero instanceof IEngineerable) {
+                    IEngineerable item = (IEngineerable) itemInSlotZero;
+                    return item.getStackInSlot(stackInSlotZero, index);
                 }
             }
             return null;
@@ -65,19 +67,19 @@ public class TileEntityEngineeringTable extends TileEntity implements IInventory
     }
 
     @Override
-    public ItemStack decrStackSize(int var1, int var2) {
-        if (var1 == 0) {
-            if (this.furnaceItemStacks[var1] != null) {
+    public ItemStack decrStackSize(int index, int count) {
+        if (index == 0) {
+            if (contents[index] != null) {
                 ItemStack itemstack;
-                if (this.furnaceItemStacks[var1].stackSize <= var2) {
-                    itemstack = this.furnaceItemStacks[var1];
-                    this.furnaceItemStacks[var1] = null;
+                if (contents[index].stackSize <= count) {
+                    itemstack = contents[index];
+                    contents[index] = null;
                     return itemstack;
                 } else {
-                    itemstack = this.furnaceItemStacks[var1].splitStack(var2);
+                    itemstack = contents[index].splitStack(count);
 
-                    if (this.furnaceItemStacks[var1].stackSize == 0) {
-                        this.furnaceItemStacks[var1] = null;
+                    if (contents[index].stackSize == 0) {
+                        contents[index] = null;
                     }
                     return itemstack;
                 }
@@ -85,10 +87,12 @@ public class TileEntityEngineeringTable extends TileEntity implements IInventory
                 return null;
             }
         } else {
-            if (this.getStackInSlot(0) != null) {
-                if (this.getStackInSlot(0).getItem() instanceof IEngineerable) {
-                    IEngineerable item = (IEngineerable) this.getStackInSlot(0).getItem();
-                    return item.decrStackSize(this.getStackInSlot(0), var1, var2);
+            ItemStack stackInSlotZero = getStackInSlot(0);
+            if (stackInSlotZero != null) {
+                Item itemInSlotZero = stackInSlotZero.getItem();
+                if (itemInSlotZero instanceof IEngineerable) {
+                    IEngineerable item = (IEngineerable) itemInSlotZero;
+                    return item.decrStackSize(stackInSlotZero, index, count);
                 }
             }
             return null;
@@ -96,42 +100,25 @@ public class TileEntityEngineeringTable extends TileEntity implements IInventory
     }
 
     @Override
-    public ItemStack getStackInSlotOnClosing(int var1) {
-        if (var1 == 0) {
-            return furnaceItemStacks[var1];
-        } else {
-            if (this.getStackInSlot(0) != null) {
-                if (this.getStackInSlot(0).getItem() instanceof IEngineerable) {
-                    IEngineerable item = (IEngineerable) this.getStackInSlot(0).getItem();
-                    return item.getStackInSlot(this.getStackInSlot(0), var1);
-                }
-            }
-            return null;
-        }
+    public ItemStack removeStackFromSlot(int index) {
+        // For some reason these two methods did exactly the same thing.
+        return getStackInSlot(index);
     }
 
     @Override
-    public void setInventorySlotContents(int var1, ItemStack var2) {
-        if (var1 == 0) {
-            furnaceItemStacks[var1] = var2;
+    public void setInventorySlotContents(int index, ItemStack stack) {
+        if (index == 0) {
+            contents[index] = stack;
         } else {
-            if (this.getStackInSlot(0) != null) {
-                if (this.getStackInSlot(0).getItem() instanceof IEngineerable) {
-                    IEngineerable item = (IEngineerable) this.getStackInSlot(0).getItem();
-                    item.setInventorySlotContents(this.getStackInSlot(0), var1, var2);
+            ItemStack stackInSlotZero = getStackInSlot(0);
+            if (stackInSlotZero != null) {
+                Item itemInSlotZero = stackInSlotZero.getItem();
+                if (itemInSlotZero instanceof IEngineerable) {
+                    IEngineerable item = (IEngineerable) itemInSlotZero;
+                    item.setInventorySlotContents(stackInSlotZero, index, stack);
                 }
             }
         }
-    }
-
-    @Override
-    public String getInventoryName() {
-        return null;
-    }
-
-    @Override
-    public boolean hasCustomInventoryName() {
-        return false;
     }
 
     @Override
@@ -140,31 +127,65 @@ public class TileEntityEngineeringTable extends TileEntity implements IInventory
     }
 
     @Override
-    public boolean isUseableByPlayer(EntityPlayer var1) {
+    public boolean isUseableByPlayer(EntityPlayer player) {
         return true;
     }
 
     @Override
-    public void openInventory() {
-    }
+    public void openInventory(EntityPlayer player) {}
 
     @Override
-    public void closeInventory() {
-    }
+    public void closeInventory(EntityPlayer player) {}
 
     @Override
-    public boolean isItemValidForSlot(int var1, ItemStack var2) {
-        if (var1 == 0) {
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        if (index == 0) {
             return true;
         } else {
-            if (this.getStackInSlot(0) != null) {
-                if (this.getStackInSlot(0).getItem() instanceof IEngineerable) {
-                    IEngineerable item = (IEngineerable) this.getStackInSlot(0).getItem();
-                    return item.isItemValidForSlot(this.getStackInSlot(0), var1, var2);
+            ItemStack stackInSlotZero = getStackInSlot(0);
+            if (stackInSlotZero != null) {
+                Item itemInSlotZero = stackInSlotZero.getItem();
+                if (itemInSlotZero instanceof IEngineerable) {
+                    IEngineerable item = (IEngineerable) itemInSlotZero;
+                    return item.isItemValidForSlot(stackInSlotZero, index, stack);
                 }
             }
             return false;
         }
     }
 
+    @Override
+    public void setField(int id, int value) {}
+
+    @Override
+    public int getFieldCount() {
+        return 0;
+    }
+
+    @Override
+    public int getField(int id) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasCustomName() {
+        return false;
+    }
+
+    @Override
+    public ITextComponent getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < contents.length; i++) {
+            contents[i] = null;
+        }
+    }
 }
