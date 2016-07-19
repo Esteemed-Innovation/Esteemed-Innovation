414f287978b67709e64315fdd9a6b8ccb662a769
elifoster <elifosterwy@gmail.com>
2016-07-18 19:46:54 -0700

    :wrench: Allow passing a type parameter to IInventoryTESR, like TileEntitySpecialRenderer.

--- src/main/java/flaxbeard/steamcraft/client/render/tile/IInventoryTESR.java
+++ src/main/java/flaxbeard/steamcraft/client/render/tile/IInventoryTESR.java
@@ -2,6 +2,6 @@ package flaxbeard.steamcraft.client.render.tile;
 
 import net.minecraft.tileentity.TileEntity;
 
-public interface IInventoryTESR {
-    void renderInventoryTileEntityAt(TileEntity tile, double x, double y, double z, float var8);
+public interface IInventoryTESR<T extends TileEntity> {
+    void renderInventoryTileEntityAt(T tile, double x, double y, double z, float var8);
 }
