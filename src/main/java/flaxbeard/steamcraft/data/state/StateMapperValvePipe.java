cb24d6e9095e029eb584b38cc28e42ef7014474a
elifoster <elifosterwy@gmail.com>
2016-07-25 18:03:07 -0700

    :fu: Clean up valve pipe state mapper

--- src/main/java/flaxbeard/steamcraft/data/state/StateMapperValvePipe.java
+++ src/main/java/flaxbeard/steamcraft/data/state/StateMapperValvePipe.java
@@ -1,22 +1,16 @@
 package flaxbeard.steamcraft.data.state;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.util.ResourceLocation;
 
 import flaxbeard.steamcraft.block.BlockValvePipe;
 import flaxbeard.steamcraft.init.blocks.SteamNetworkBlocks;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-
-import javax.annotation.Nullable;
 
 public class StateMapperValvePipe extends StateMapperBase {
     private static final ResourceLocation PIPE_NAME = SteamNetworkBlocks.Blocks.PIPE.getBlock().getRegistryName();
