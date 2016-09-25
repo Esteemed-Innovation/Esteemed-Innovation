0e1fe665c5cb3edd3d90eb850e973e60a62185f7
elifoster <elifosterwy@gmail.com>
2016-09-24 23:19:17 -0700

    :fu: Remove areBlockPosEqual because BlockPos#equals does the same thing and is more weakly typed

--- src/main/java/eiteam/esteemedinnovation/misc/WorldHelper.java
+++ src/main/java/eiteam/esteemedinnovation/misc/WorldHelper.java
@@ -1,20 +1,10 @@
 package eiteam.esteemedinnovation.misc;
 
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.BlockPos;
 
 import java.util.Random;
 
 public class WorldHelper {
-    /**
-     * @param first BlockPos
-     * @param second BlockPos
-     * @return Returns whether the two BlockPos have equal X, Y, and Z values.
-     */
-    public static boolean areBlockPosEqual(BlockPos first, BlockPos second) {
-        return first.getX() == second.getX() && first.getY() == second.getY() && first.getZ() == second.getZ();
-    }
-
     /**
      * @param random Random object
      * @return A random value of North, South, East, or West. Like {@link EnumFacing#random(Random)} but does not include
