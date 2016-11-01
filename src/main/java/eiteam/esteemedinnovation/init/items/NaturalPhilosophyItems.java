1f35259cdce363e94b5c37db360227dea4e0fd29
elifoster <elifosterwy@gmail.com>
2016-11-01 11:12:53 -0700

    :gem: Natural Philosophy recipes. Close #466.

--- src/main/java/eiteam/esteemedinnovation/init/items/NaturalPhilosophyItems.java
+++ src/main/java/eiteam/esteemedinnovation/init/items/NaturalPhilosophyItems.java
@@ -6,15 +6,19 @@ import eiteam.esteemedinnovation.init.misc.OreDictEntries;
 import eiteam.esteemedinnovation.item.ItemKitBag;
 import eiteam.esteemedinnovation.item.ItemResearchLog;
 import eiteam.esteemedinnovation.item.ItemSoilSamplingKit;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
-import static net.minecraft.init.Items.BOOK;
+import static net.minecraft.init.Items.*;
 
 public class NaturalPhilosophyItems implements IInitCategory {
     public enum Items {
-        SOIL_SAPLING_KIT(new ItemSoilSamplingKit(), "soil_sampling_kit"),
+        SOIL_SAMPLING_KIT(new ItemSoilSamplingKit(), "soil_sampling_kit"),
         BIOME_LOG(new ItemResearchLog(), "research_log_biome"),
         KIT_BAG(new ItemKitBag(), "kit_bag");
 
@@ -49,9 +53,33 @@ public class NaturalPhilosophyItems implements IInitCategory {
     public void recipes() {
         for (Items item : Items.LOOKUP) {
             switch (item) {
-                case SOIL_SAPLING_KIT: {
+                case BIOME_LOG: {
                     GameRegistry.addRecipe(new ShapelessOreRecipe(item.getItem(), BOOK,
                       OreDictEntries.DYE_WHITE, OreDictEntries.DYE_WHITE));
+                    break;
+                }
+                case SOIL_SAMPLING_KIT: {
+                    GameRegistry.addRecipe(new ShapedOreRecipe(item.getItem(),
+                      "MKS",
+                      "WIW",
+                      " W ",
+                      'M', OreDictEntries.DYE_WHITE,
+                      'K', Items.KIT_BAG.getItem(),
+                      'S', SUGAR,
+                      'W', POTIONITEM,
+                      'I', IRON_SHOVEL
+                    ));
+                    break;
+                }
+                case KIT_BAG: {
+                    GameRegistry.addRecipe(new ShapedOreRecipe(item.getItem(),
+                      "SSS",
+                      "LWL",
+                      " L ",
+                      'S', OreDictEntries.STRING_ORE,
+                      'L', OreDictEntries.LEATHER_ORE,
+                      'W', new ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE)));
+                    break;
                 }
             }
         }
