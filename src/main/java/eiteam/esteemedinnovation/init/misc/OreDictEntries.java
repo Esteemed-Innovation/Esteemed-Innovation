13677bd29e966ba2b8b2ddd9463a3f14f167ada3
elifoster <elifosterwy@gmail.com>
2016-12-14 21:39:13 -0800

    :fu: OreDictEntrise are final

--- src/main/java/eiteam/esteemedinnovation/init/misc/OreDictEntries.java
+++ src/main/java/eiteam/esteemedinnovation/init/misc/OreDictEntries.java
@@ -2,187 +2,187 @@ package eiteam.esteemedinnovation.init.misc;
 
 public class OreDictEntries {
     // Prefixes.
-    public static String PREFIX_BLOCK = "block";
-    public static String PREFIX_INGOT = "ingot";
-    public static String PREFIX_PLATE_THIN = "plateThin";
-    public static String PREFIX_NUGGET = "nugget";
-    public static String PREFIX_PLANK = "plank";
-    public static String PREFIX_DUST = "dust";
-    public static String PREFIX_STICK = "stick";
-    public static String PREFIX_GEM = "gem";
-    public static String PREFIX_ORE = "ore";
-    public static String PREFIX_DUST_TINY = "dustTiny";
-    public static String PREFIX_PANE = "pane";
-    public static String PREFIX_TREE = "tree";
-    public static String PREFIX_LOG = "log";
-    public static String PREFIX_SLAB = "slab";
-    public static String PREFIX_DYE = "dye";
+    public static final String PREFIX_BLOCK = "block";
+    public static final String PREFIX_INGOT = "ingot";
+    public static final String PREFIX_PLATE_THIN = "plateThin";
+    public static final String PREFIX_NUGGET = "nugget";
+    public static final String PREFIX_PLANK = "plank";
+    public static final String PREFIX_DUST = "dust";
+    public static final String PREFIX_STICK = "stick";
+    public static final String PREFIX_GEM = "gem";
+    public static final String PREFIX_ORE = "ore";
+    public static final String PREFIX_DUST_TINY = "dustTiny";
+    public static final String PREFIX_PANE = "pane";
+    public static final String PREFIX_TREE = "tree";
+    public static final String PREFIX_LOG = "log";
+    public static final String PREFIX_SLAB = "slab";
+    public static final String PREFIX_DYE = "dye";
     // Primarily used by Harvestcraft.
-    public static String PREFIX_LIST_ALL = "listAll";
+    public static final String PREFIX_LIST_ALL = "listAll";
 
     // Materials.
-    public static String MATERIAL_BRASS = "Brass";
-    public static String MATERIAL_COPPER = "Copper";
-    public static String MATERIAL_ZINC = "Zinc";
-    public static String MATERIAL_GILDED_IRON = "GildedIron";
-    public static String MATERIAL_IRON = "Iron";
-    public static String MATERIAL_GOLD = "Gold";
-    public static String MATERIAL_LEAD = "Lead";
-    public static String MATERIAL_STEEL = "Steel";
-    public static String MATERIAL_SILVER = "Silver";
-    public static String MATERIAL_WOOD = "Wood";
-    public static String MATERIAL_REDSTONE = "Redstone";
-    public static String MATERIAL_DIAMOND = "Diamond";
-    public static String MATERIAL_GLASS = "Glass";
-    public static String MATERIAL_GLASS_COLORLESS = MATERIAL_GLASS + "Colorless";
-    public static String MATERIAL_EMERALD = "Emerald";
-    public static String MATERIAL_LEAVES = "Leaves";
-    public static String MATERIAL_COAL = "Coal";
-    public static String MATERIAL_SAPLING = "Sapling";
+    public static final String MATERIAL_BRASS = "Brass";
+    public static final String MATERIAL_COPPER = "Copper";
+    public static final String MATERIAL_ZINC = "Zinc";
+    public static final String MATERIAL_GILDED_IRON = "GildedIron";
+    public static final String MATERIAL_IRON = "Iron";
+    public static final String MATERIAL_GOLD = "Gold";
+    public static final String MATERIAL_LEAD = "Lead";
+    public static final String MATERIAL_STEEL = "Steel";
+    public static final String MATERIAL_SILVER = "Silver";
+    public static final String MATERIAL_WOOD = "Wood";
+    public static final String MATERIAL_REDSTONE = "Redstone";
+    public static final String MATERIAL_DIAMOND = "Diamond";
+    public static final String MATERIAL_GLASS = "Glass";
+    public static final String MATERIAL_GLASS_COLORLESS = MATERIAL_GLASS + "Colorless";
+    public static final String MATERIAL_EMERALD = "Emerald";
+    public static final String MATERIAL_LEAVES = "Leaves";
+    public static final String MATERIAL_COAL = "Coal";
+    public static final String MATERIAL_SAPLING = "Sapling";
 
     // Colors.
-    public static String COLOR_WHITE = "White";
-    public static String COLOR_ORANGE = "Orange";
-    public static String COLOR_MAGENTA = "Magenta";
-    public static String COLOR_LIGHT_BLUE = "LightBlue";
-    public static String COLOR_YELLOW = "Yellow";
-    public static String COLOR_LIME = "Lime";
-    public static String COLOR_PINK = "Pink";
-    public static String COLOR_GRAY = "Gray";
-    public static String COLOR_SILVER = "Silver";
-    public static String COLOR_CYAN = "Cyan";
-    public static String COLOR_PURPLE = "Purple";
-    public static String COLOR_BLUE = "Blue";
-    public static String COLOR_BROWN = "Brown";
-    public static String COLOR_GREEN = "Green";
-    public static String COLOR_RED = "Red";
-    public static String COLOR_BLACK = "Black";
+    public static final String COLOR_WHITE = "White";
+    public static final String COLOR_ORANGE = "Orange";
+    public static final String COLOR_MAGENTA = "Magenta";
+    public static final String COLOR_LIGHT_BLUE = "LightBlue";
+    public static final String COLOR_YELLOW = "Yellow";
+    public static final String COLOR_LIME = "Lime";
+    public static final String COLOR_PINK = "Pink";
+    public static final String COLOR_GRAY = "Gray";
+    public static final String COLOR_SILVER = "Silver";
+    public static final String COLOR_CYAN = "Cyan";
+    public static final String COLOR_PURPLE = "Purple";
+    public static final String COLOR_BLUE = "Blue";
+    public static final String COLOR_BROWN = "Brown";
+    public static final String COLOR_GREEN = "Green";
+    public static final String COLOR_RED = "Red";
+    public static final String COLOR_BLACK = "Black";
 
     // Foods.
-    public static String FOOD_COOKED_FISH = "fishcooked";
-    public static String FOOD_COOKED_CHICKEN = "chickencooked";
-    public static String FOOD_COOKED_BEEF = "beefcooked";
-    public static String FOOD_COOKED_PORK = "porkcooked";
-    public static String FOOD_COOKED_MEAT = "meatcooked";
-    public static String FOOD_VEGGIE = "veggie";
+    public static final String FOOD_COOKED_FISH = "fishcooked";
+    public static final String FOOD_COOKED_CHICKEN = "chickencooked";
+    public static final String FOOD_COOKED_BEEF = "beefcooked";
+    public static final String FOOD_COOKED_PORK = "porkcooked";
+    public static final String FOOD_COOKED_MEAT = "meatcooked";
+    public static final String FOOD_VEGGIE = "veggie";
 
     // Blocks.
-    public static String BLOCK_BRASS = PREFIX_BLOCK + MATERIAL_BRASS;
-    public static String BLOCK_COPPER = PREFIX_BLOCK + MATERIAL_COPPER;
-    public static String BLOCK_ZINC = PREFIX_BLOCK + MATERIAL_ZINC;
-    public static String BLOCK_GILDED_IRON = PREFIX_BLOCK + MATERIAL_GILDED_IRON;
-    public static String BLOCK_IRON = PREFIX_BLOCK + MATERIAL_IRON;
-    public static String BLOCK_GLASS = PREFIX_BLOCK + MATERIAL_GLASS;
-    public static String BLOCK_GLASS_COLORLESS = PREFIX_BLOCK + MATERIAL_GLASS_COLORLESS;
-    public static String BLOCK_EMERALD = PREFIX_BLOCK + MATERIAL_EMERALD;
+    public static final String BLOCK_BRASS = PREFIX_BLOCK + MATERIAL_BRASS;
+    public static final String BLOCK_COPPER = PREFIX_BLOCK + MATERIAL_COPPER;
+    public static final String BLOCK_ZINC = PREFIX_BLOCK + MATERIAL_ZINC;
+    public static final String BLOCK_GILDED_IRON = PREFIX_BLOCK + MATERIAL_GILDED_IRON;
+    public static final String BLOCK_IRON = PREFIX_BLOCK + MATERIAL_IRON;
+    public static final String BLOCK_GLASS = PREFIX_BLOCK + MATERIAL_GLASS;
+    public static final String BLOCK_GLASS_COLORLESS = PREFIX_BLOCK + MATERIAL_GLASS_COLORLESS;
+    public static final String BLOCK_EMERALD = PREFIX_BLOCK + MATERIAL_EMERALD;
 
     // Ingots.
-    public static String INGOT_BRASS = PREFIX_INGOT + MATERIAL_BRASS;
-    public static String INGOT_COPPER = PREFIX_INGOT + MATERIAL_COPPER;
-    public static String INGOT_ZINC = PREFIX_INGOT + MATERIAL_ZINC;
-    public static String INGOT_GILDED_IRON = PREFIX_INGOT + MATERIAL_GILDED_IRON;
-    public static String INGOT_IRON = PREFIX_INGOT + MATERIAL_IRON;
-    public static String INGOT_GOLD = PREFIX_INGOT + MATERIAL_GOLD;
-    public static String INGOT_LEAD = PREFIX_INGOT + MATERIAL_LEAD;
+    public static final String INGOT_BRASS = PREFIX_INGOT + MATERIAL_BRASS;
+    public static final String INGOT_COPPER = PREFIX_INGOT + MATERIAL_COPPER;
+    public static final String INGOT_ZINC = PREFIX_INGOT + MATERIAL_ZINC;
+    public static final String INGOT_GILDED_IRON = PREFIX_INGOT + MATERIAL_GILDED_IRON;
+    public static final String INGOT_IRON = PREFIX_INGOT + MATERIAL_IRON;
+    public static final String INGOT_GOLD = PREFIX_INGOT + MATERIAL_GOLD;
+    public static final String INGOT_LEAD = PREFIX_INGOT + MATERIAL_LEAD;
 
     // Thin plates.
-    public static String PLATE_THIN_BRASS = PREFIX_PLATE_THIN + MATERIAL_BRASS;
-    public static String PLATE_THIN_COPPER = PREFIX_PLATE_THIN + MATERIAL_COPPER;
-    public static String PLATE_THIN_ZINC = PREFIX_PLATE_THIN + MATERIAL_ZINC;
-    public static String PLATE_THIN_GILDED_IRON = PREFIX_PLATE_THIN + MATERIAL_GILDED_IRON;
-    public static String PLATE_THIN_IRON = PREFIX_PLATE_THIN + MATERIAL_IRON;
-    public static String PLATE_THIN_GOLD = PREFIX_PLATE_THIN + MATERIAL_GOLD;
-    public static String PLATE_THIN_LEAD = PREFIX_PLATE_THIN + MATERIAL_LEAD;
+    public static final String PLATE_THIN_BRASS = PREFIX_PLATE_THIN + MATERIAL_BRASS;
+    public static final String PLATE_THIN_COPPER = PREFIX_PLATE_THIN + MATERIAL_COPPER;
+    public static final String PLATE_THIN_ZINC = PREFIX_PLATE_THIN + MATERIAL_ZINC;
+    public static final String PLATE_THIN_GILDED_IRON = PREFIX_PLATE_THIN + MATERIAL_GILDED_IRON;
+    public static final String PLATE_THIN_IRON = PREFIX_PLATE_THIN + MATERIAL_IRON;
+    public static final String PLATE_THIN_GOLD = PREFIX_PLATE_THIN + MATERIAL_GOLD;
+    public static final String PLATE_THIN_LEAD = PREFIX_PLATE_THIN + MATERIAL_LEAD;
 
     // Nuggets.
-    public static String NUGGET_BRASS = PREFIX_NUGGET + MATERIAL_BRASS;
-    public static String NUGGET_COPPER = PREFIX_NUGGET + MATERIAL_COPPER;
-    public static String NUGGET_ZINC = PREFIX_NUGGET + MATERIAL_ZINC;
-    public static String NUGGET_GILDED_IRON = PREFIX_NUGGET + MATERIAL_GILDED_IRON;
-    public static String NUGGET_IRON = PREFIX_NUGGET + MATERIAL_IRON;
-    public static String NUGGET_LEAD = PREFIX_NUGGET + MATERIAL_LEAD;
-    public static String NUGGET_STEEL = PREFIX_NUGGET + MATERIAL_STEEL;
-    public static String NUGGET_SILVER = PREFIX_NUGGET + MATERIAL_SILVER;
-    public static String NUGGET_GOLD = PREFIX_NUGGET + MATERIAL_GOLD;
+    public static final String NUGGET_BRASS = PREFIX_NUGGET + MATERIAL_BRASS;
+    public static final String NUGGET_COPPER = PREFIX_NUGGET + MATERIAL_COPPER;
+    public static final String NUGGET_ZINC = PREFIX_NUGGET + MATERIAL_ZINC;
+    public static final String NUGGET_GILDED_IRON = PREFIX_NUGGET + MATERIAL_GILDED_IRON;
+    public static final String NUGGET_IRON = PREFIX_NUGGET + MATERIAL_IRON;
+    public static final String NUGGET_LEAD = PREFIX_NUGGET + MATERIAL_LEAD;
+    public static final String NUGGET_STEEL = PREFIX_NUGGET + MATERIAL_STEEL;
+    public static final String NUGGET_SILVER = PREFIX_NUGGET + MATERIAL_SILVER;
+    public static final String NUGGET_GOLD = PREFIX_NUGGET + MATERIAL_GOLD;
 
     // Ores.
-    public static String ORE_COPPER = PREFIX_ORE + MATERIAL_COPPER;
-    public static String ORE_ZINC = PREFIX_ORE + MATERIAL_ZINC;
+    public static final String ORE_COPPER = PREFIX_ORE + MATERIAL_COPPER;
+    public static final String ORE_ZINC = PREFIX_ORE + MATERIAL_ZINC;
 
     // Planks.
-    public static String PLANK_WOOD = PREFIX_PLANK + MATERIAL_WOOD;
+    public static final String PLANK_WOOD = PREFIX_PLANK + MATERIAL_WOOD;
 
     // Dusts.
-    public static String DUST_REDSTONE = PREFIX_DUST + MATERIAL_REDSTONE;
-    public static String DUST_GOLD = PREFIX_DUST + MATERIAL_GOLD;
-    public static String DUST_IRON = PREFIX_DUST + MATERIAL_IRON;
-    public static String DUST_COPPER = PREFIX_DUST + MATERIAL_COPPER;
-    public static String DUST_ZINC = PREFIX_DUST + MATERIAL_ZINC;
-    public static String DUST_BRASS = PREFIX_DUST + MATERIAL_BRASS;
-    public static String DUST_LEAD = PREFIX_DUST + MATERIAL_LEAD;
+    public static final String DUST_REDSTONE = PREFIX_DUST + MATERIAL_REDSTONE;
+    public static final String DUST_GOLD = PREFIX_DUST + MATERIAL_GOLD;
+    public static final String DUST_IRON = PREFIX_DUST + MATERIAL_IRON;
+    public static final String DUST_COPPER = PREFIX_DUST + MATERIAL_COPPER;
+    public static final String DUST_ZINC = PREFIX_DUST + MATERIAL_ZINC;
+    public static final String DUST_BRASS = PREFIX_DUST + MATERIAL_BRASS;
+    public static final String DUST_LEAD = PREFIX_DUST + MATERIAL_LEAD;
 
     // Tiny Dusts.
-    public static String DUST_TINY_GOLD = PREFIX_DUST_TINY + MATERIAL_GOLD;
-    public static String DUST_TINY_IRON = PREFIX_DUST_TINY + MATERIAL_IRON;
-    public static String DUST_TINY_ZINC = PREFIX_DUST_TINY + MATERIAL_ZINC;
-    public static String DUST_TINY_COPPER = PREFIX_DUST_TINY + MATERIAL_COPPER;
-    public static String DUST_TINY_BRASS = PREFIX_DUST_TINY + MATERIAL_BRASS;
+    public static final String DUST_TINY_GOLD = PREFIX_DUST_TINY + MATERIAL_GOLD;
+    public static final String DUST_TINY_IRON = PREFIX_DUST_TINY + MATERIAL_IRON;
+    public static final String DUST_TINY_ZINC = PREFIX_DUST_TINY + MATERIAL_ZINC;
+    public static final String DUST_TINY_COPPER = PREFIX_DUST_TINY + MATERIAL_COPPER;
+    public static final String DUST_TINY_BRASS = PREFIX_DUST_TINY + MATERIAL_BRASS;
 
     // Sticks.
-    public static String STICK_WOOD = PREFIX_STICK + MATERIAL_WOOD;
+    public static final String STICK_WOOD = PREFIX_STICK + MATERIAL_WOOD;
 
     // Gems.
-    public static String GEM_DIAMOND = PREFIX_GEM + MATERIAL_DIAMOND;
-    public static String GEM_EMERALD = PREFIX_GEM + MATERIAL_EMERALD;
+    public static final String GEM_DIAMOND = PREFIX_GEM + MATERIAL_DIAMOND;
+    public static final String GEM_EMERALD = PREFIX_GEM + MATERIAL_EMERALD;
 
     // Panes.
-    public static String PANE_GLASS = PREFIX_PANE + MATERIAL_GLASS;
-    public static String PANE_GLASS_COLORLESS = PREFIX_PANE + MATERIAL_GLASS_COLORLESS;
+    public static final String PANE_GLASS = PREFIX_PANE + MATERIAL_GLASS;
+    public static final String PANE_GLASS_COLORLESS = PREFIX_PANE + MATERIAL_GLASS_COLORLESS;
 
     // Harvestcraft foods.
-    public static String ALL_FISH_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_FISH;
-    public static String ALL_CHICKEN_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_CHICKEN;
-    public static String ALL_BEEF_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_BEEF;
-    public static String ALL_PORK_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_PORK;
-    public static String ALL_MEAT_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_MEAT;
-    public static String ALL_VEGGIE = PREFIX_LIST_ALL + FOOD_VEGGIE;
+    public static final String ALL_FISH_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_FISH;
+    public static final String ALL_CHICKEN_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_CHICKEN;
+    public static final String ALL_BEEF_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_BEEF;
+    public static final String ALL_PORK_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_PORK;
+    public static final String ALL_MEAT_COOKED = PREFIX_LIST_ALL + FOOD_COOKED_MEAT;
+    public static final String ALL_VEGGIE = PREFIX_LIST_ALL + FOOD_VEGGIE;
 
     // Dyes.
-    public static String DYE_WHITE = PREFIX_DYE + COLOR_WHITE;
-    public static String DYE_ORANGE = PREFIX_DYE + COLOR_WHITE;
-    public static String DYE_MAGENTA = PREFIX_DYE + COLOR_MAGENTA;
-    public static String DYE_LIGHT_BLUE = PREFIX_DYE + COLOR_LIGHT_BLUE;
-    public static String DYE_YELLOW = PREFIX_DYE + COLOR_YELLOW;
-    public static String DYE_LIME = PREFIX_DYE + COLOR_LIME;
-    public static String DYE_PINK = PREFIX_DYE + COLOR_PINK;
-    public static String DYE_GRAY = PREFIX_DYE + COLOR_GRAY;
-    public static String DYE_SILVER = PREFIX_DYE + COLOR_SILVER;
-    public static String DYE_CYAN = PREFIX_DYE + COLOR_CYAN;
-    public static String DYE_PURPLE = PREFIX_DYE + COLOR_PURPLE;
-    public static String DYE_BLUE = PREFIX_DYE + COLOR_BLUE;
-    public static String DYE_BROWN = PREFIX_DYE + COLOR_BROWN;
-    public static String DYE_GREEN = PREFIX_DYE + COLOR_GREEN;
-    public static String DYE_RED = PREFIX_DYE + COLOR_RED;
-    public static String DYE_BLACK = PREFIX_DYE + COLOR_BLACK;
+    public static final String DYE_WHITE = PREFIX_DYE + COLOR_WHITE;
+    public static final String DYE_ORANGE = PREFIX_DYE + COLOR_WHITE;
+    public static final String DYE_MAGENTA = PREFIX_DYE + COLOR_MAGENTA;
+    public static final String DYE_LIGHT_BLUE = PREFIX_DYE + COLOR_LIGHT_BLUE;
+    public static final String DYE_YELLOW = PREFIX_DYE + COLOR_YELLOW;
+    public static final String DYE_LIME = PREFIX_DYE + COLOR_LIME;
+    public static final String DYE_PINK = PREFIX_DYE + COLOR_PINK;
+    public static final String DYE_GRAY = PREFIX_DYE + COLOR_GRAY;
+    public static final String DYE_SILVER = PREFIX_DYE + COLOR_SILVER;
+    public static final String DYE_CYAN = PREFIX_DYE + COLOR_CYAN;
+    public static final String DYE_PURPLE = PREFIX_DYE + COLOR_PURPLE;
+    public static final String DYE_BLUE = PREFIX_DYE + COLOR_BLUE;
+    public static final String DYE_BROWN = PREFIX_DYE + COLOR_BROWN;
+    public static final String DYE_GREEN = PREFIX_DYE + COLOR_GREEN;
+    public static final String DYE_RED = PREFIX_DYE + COLOR_RED;
+    public static final String DYE_BLACK = PREFIX_DYE + COLOR_BLACK;
 
     // Tree stuff.
-    public static String TREE_LEAVES = PREFIX_TREE + MATERIAL_LEAVES;
-    public static String LOG_WOOD = PREFIX_LOG + MATERIAL_WOOD;
-    public static String SLAB_WOOD = PREFIX_SLAB + MATERIAL_WOOD;
-    public static String TREE_SAPLING = PREFIX_TREE + MATERIAL_SAPLING;
+    public static final String TREE_LEAVES = PREFIX_TREE + MATERIAL_LEAVES;
+    public static final String LOG_WOOD = PREFIX_LOG + MATERIAL_WOOD;
+    public static final String SLAB_WOOD = PREFIX_SLAB + MATERIAL_WOOD;
+    public static final String TREE_SAPLING = PREFIX_TREE + MATERIAL_SAPLING;
 
 
     // Disambiguated vanilla ore entries.
-    // These entries would conflict with their Items/Blocks counterparts when statically imported.
-    public static String COBBLESTONE_ORE = "cobblestone";
-    public static String SLIMEBALL_ORE = "slimeball";
-    public static String LEATHER_ORE = "leather";
-    public static String STRING_ORE = "string";
-    public static String GRAVEL_ORE = "gravel";
-    public static String GLOWSTONE_ORE = "glowstone";
-    public static String SANDSTONE_ORE = "sandstone";
-    public static String STONE_ORE = "stone";
-    public static String SAND_ORE = "sand";
-    public static String DIRT_ORE = "dirt";
-    public static String GRASS_ORE = "grass";
+    // These entries would conflict with their Items/Blocks counterparts when static finalally imported.
+    public static final String COBBLESTONE_ORE = "cobblestone";
+    public static final String SLIMEBALL_ORE = "slimeball";
+    public static final String LEATHER_ORE = "leather";
+    public static final String STRING_ORE = "string";
+    public static final String GRAVEL_ORE = "gravel";
+    public static final String GLOWSTONE_ORE = "glowstone";
+    public static final String SANDSTONE_ORE = "sandstone";
+    public static final String STONE_ORE = "stone";
+    public static final String SAND_ORE = "sand";
+    public static final String DIRT_ORE = "dirt";
+    public static final String GRASS_ORE = "grass";
 }
