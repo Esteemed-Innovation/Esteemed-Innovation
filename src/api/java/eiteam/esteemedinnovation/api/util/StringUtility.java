0917c1c91c5e9936d2b396741eb347c379812076
elifoster <elifosterwy@gmail.com>
2017-03-16 20:37:54 -0700

    :bug: Fix StringUtility locale issues. Close #484

--- src/api/java/eiteam/esteemedinnovation/api/util/StringUtility.java
+++ src/api/java/eiteam/esteemedinnovation/api/util/StringUtility.java
@@ -1,5 +1,7 @@
 package eiteam.esteemedinnovation.api.util;
 
+import java.util.Locale;
+
 public class StringUtility {
     /**
      * Capitalizes the first letter in the provided String.
@@ -10,13 +12,13 @@ public class StringUtility {
      * capitalize("él") // => "Él"
      */
     public static String capitalize(String str) {
-        return str.substring(0, 1).toUpperCase() + (str.substring(1));
+        return str.substring(0, 1).toUpperCase(Locale.US) + (str.substring(1));
     }
 
     /**
      * Opposite of {@link StringUtility#capitalize(String)}.
      */
     public static String decapitalize(String str) {
-        return str.substring(0, 1).toLowerCase() + (str.substring(1));
+        return str.substring(0, 1).toLowerCase(Locale.US) + (str.substring(1));
     }
 }
