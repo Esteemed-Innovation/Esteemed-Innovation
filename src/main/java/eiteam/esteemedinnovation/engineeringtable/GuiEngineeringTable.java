56ad38b6f92eef32288fb5d7dc8191ad362618a1
elifoster <elifosterwy@gmail.com>
2018-03-29 21:37:03 -0700

    🐛 Fix missing tooltips and background in Engineering Table GUI Close #518.

--- src/main/java/eiteam/esteemedinnovation/engineeringtable/GuiEngineeringTable.java
+++ src/main/java/eiteam/esteemedinnovation/engineeringtable/GuiEngineeringTable.java
@@ -20,6 +20,13 @@ public class GuiEngineeringTable extends GuiContainer {
     }
 
     @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        renderHoveredToolTip(mouseX, mouseY);
+    }
+
+    @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {}
 
     @Override
